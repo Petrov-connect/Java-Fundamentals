60fe7069387fd5a10a9e2bb553f7689e2db6e6b2
Petrov-connect <Petrov.connect@gmail.com>
2021-01-05 17:45:40 +0200

    java code

--- Arrays/DayOfWeek.java
+++ Arrays/DayOfWeek.java
@@ -1,16 +1,20 @@
 package Arrays;
 //created by J.M.
+
 import java.util.Scanner;
 
 public class DayOfWeek {
+
     public static void main(String[] args) {
-         Scanner scan = new Scanner(System.in);
-            int num = Integer.parseInt(scan.nextLine());
-              String [] days = new String[] {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
-              if(1<=num&&num<=7)   {
-                  System.out.println(days[num-1]);
-              }    else{
-                  System.out.println("Invalid day!");
-              }
+
+        Scanner scan = new Scanner(System.in);
+        int num = Integer.parseInt(scan.nextLine());
+
+        String[] days = new String[]{"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+        if (1 <= num && num <= 7) {
+            System.out.println(days[num - 1]);
+        } else {
+            System.out.println("Invalid day!");
+        }
     }
 }
