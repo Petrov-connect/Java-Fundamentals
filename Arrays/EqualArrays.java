1a3272d92b3bbc69e6831de1dfae42ff81ab417a
Petrov-connect <Petrov.connect@gmail.com>
2021-01-05 17:46:00 +0200

    java code

--- Arrays/EqualArrays.java
+++ Arrays/EqualArrays.java
@@ -1,24 +1,30 @@
 package Arrays;
 //created by J.M.
+
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class EqualArrays {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
         int[] firstArray = Arrays.stream(scan.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        int [] secondArray= Arrays.stream(scan.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        boolean equals=true; int sum=0;
+        int[] secondArray = Arrays.stream(scan.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        boolean equals = true;
+        int sum = 0;
         for (int i = 0; i < firstArray.length; i++) {
             if (!(firstArray[i] == secondArray[i])) {
-                System.out.printf("Arrays are not identical. Found difference at %d index.",i);
-                equals=false; break;
-            }else{
-                sum+=firstArray[i];
+                System.out.printf("Arrays are not identical. Found difference at %d index.", i);
+                equals = false;
+                break;
+            } else {
+                sum += firstArray[i];
             }
         }
-        if(equals){
-            System.out.printf("Arrays are identical. Sum: %d",sum);
+        if (equals) {
+            System.out.printf("Arrays are identical. Sum: %d", sum);
         }
     }
 }
