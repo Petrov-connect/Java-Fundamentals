6751487fa996707e1984c02b4df8ddc0ee692464
Petrov-connect <Petrov.connect@gmail.com>
2021-01-05 17:45:05 +0200

    java code

--- Arrays/CommonElements.java
+++ Arrays/CommonElements.java
@@ -1,13 +1,17 @@
 package Arrays;
 //created by J.M.
+
 import java.util.Scanner;
 
 public class CommonElements {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
-        String[] first=scan.nextLine().split(" ");
-        String[] second=scan.nextLine().split(" ");
-        StringBuilder equals= new StringBuilder();
+        String[] first = scan.nextLine().split(" ");
+        String[] second = scan.nextLine().split(" ");
+
+        StringBuilder equals = new StringBuilder();
         for (String s : second) {
             for (String value : first) {
                 if (s.equals(value)) {
