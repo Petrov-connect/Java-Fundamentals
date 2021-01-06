1d27d5b29ff8894eb4ca639ee0a3e998d48cecbe
Petrov-connect <Petrov.connect@gmail.com>
2021-01-06 18:03:45 +0200

    java code

--- Arrays/LongestIncreasingSubsequence.java
+++ Arrays/LongestIncreasingSubsequence.java
@@ -1,33 +1,43 @@
 package Arrays;
 //created by J.M.
+
 import java.util.Arrays;
 import java.util.Scanner;
 
 class LongestIncreasingSubsequence {
-    public static void main (String[] args) {
+
+    public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
-        int [] numbers= Arrays.stream(scan.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        int maxLength = 0; int lastIndex = -1;
+        int[] numbers = Arrays.stream(scan.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        int maxLength = 0;
+        int lastIndex = -1;
         int[] len = new int[numbers.length];
         int[] previous = new int[numbers.length];
+
         for (int i = 0; i < numbers.length; i++) {
-            len[i] = 1; previous[i] = -1;
+            len[i] = 1;
+            previous[i] = -1;
             for (int k = 0; k < i; k++) {
                 if (numbers[k] < numbers[i] && len[k] + 1 > len[i]) {
-                    len[i] = len[k] + 1; previous[i] = k;
+                    len[i] = len[k] + 1;
+                    previous[i] = k;
                 }
             }
-            if (len[i] > maxLength) { maxLength = len[i]; lastIndex = i;
+            if (len[i] > maxLength) {
+                maxLength = len[i];
+                lastIndex = i;
             }
         }
         int[] lis = new int[maxLength];
         int currentIndex = maxLength - 1;
-        while (lastIndex != -1)  {
-            lis[currentIndex] = numbers[lastIndex]; currentIndex--; lastIndex = previous[lastIndex];
+        while (lastIndex != -1) {
+            lis[currentIndex] = numbers[lastIndex];
+            currentIndex--;
+            lastIndex = previous[lastIndex];
         }
-        for (int li : lis) {
-            System.out.print(li + " ");
-        }
+        Arrays.stream(lis).mapToObj(li -> li + " ").forEach(System.out::print);
     }
 }
 
