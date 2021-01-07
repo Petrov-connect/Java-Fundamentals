e1e0e496ceb4e1182c4a896b4d0541a9837b3898
Petrov-connect <Petrov.connect@gmail.com>
2021-01-07 23:37:19 +0200

    java code

--- Arrays/ReverseArrayOfStrings.java
+++ Arrays/ReverseArrayOfStrings.java
@@ -1,16 +1,20 @@
 package Arrays;
 //created by J.M.
+
 import java.util.Scanner;
 
 public class ReverseArrayOfStrings {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
-        String[] input=scan.nextLine().split(" ");
-        for(int i=0; i<input.length/2; i++){
-            String oldInput=input[i];
-            input[i]=input[input.length-1-i];
-            input[input.length-1-i]=oldInput;
+        String[] input = scan.nextLine().split(" ");
+
+        for (int i = 0; i < input.length / 2; i++) {
+            String oldInput = input[i];
+            input[i] = input[input.length - 1 - i];
+            input[input.length - 1 - i] = oldInput;
         }
-        System.out.print(String.join(" ",input));
+        System.out.print(String.join(" ", input));
     }
 }
