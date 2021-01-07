850521824abf0a69532ef305713d2517fc7da171
Petrov-connect <Petrov.connect@gmail.com>
2021-01-07 23:37:42 +0200

    java code

--- Arrays/TopIntegers.java
+++ Arrays/TopIntegers.java
@@ -1,25 +1,29 @@
 package Arrays;
 //created by J.M.
+
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class TopIntegers {
+
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int [] arr= Arrays.stream(scan.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        StringBuilder output=new StringBuilder();
-        for (int i=0; i<arr.length; i++){
-            boolean isBigger=true;
-            for (int j=i+1; j<arr.length; j++){
+        int[] arr = Arrays.stream(scan.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        StringBuilder output = new StringBuilder();
+
+        for (int i = 0; i < arr.length; i++) {
+            boolean isBigger = true;
+            for (int j = i + 1; j < arr.length; j++) {
                 if (!(arr[i] > arr[j])) {
                     isBigger = false;
                     break;
                 }
             }
-            if(isBigger){
+            if (isBigger) {
                 output.append(arr[i]).append(" ");
             }
         }
-        System.out.println(String.join(" ",output));
+        System.out.println(String.join(" ", output));
     }
 }
