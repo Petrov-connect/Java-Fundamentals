642af4103ab6a75915c74fab86a6924a2d7092ec
Petrov-connect <Petrov.connect@gmail.com>
2021-01-07 23:37:30 +0200

    java code

--- Arrays/SumEvenNumbers.java
+++ Arrays/SumEvenNumbers.java
@@ -1,13 +1,18 @@
 package Arrays;
 //created by J.M.
+
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class SumEvenNumbers {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
-        int [] arr= Arrays.stream(scan.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        int sum=0;
+        int[] arr = Arrays.stream(scan.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        int sum = 0;
+
         for (int i : arr) {
             if (i % 2 == 0) {
                 sum += i;
