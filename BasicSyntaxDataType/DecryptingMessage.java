6981c41f416c662a305152110fe0c6274d757b77
Petrov-connect <Petrov.connect@gmail.com>
2021-01-08 16:01:42 +0200

    java code

--- BasicSyntaxDataType/DecryptingMessage.java
+++ BasicSyntaxDataType/DecryptingMessage.java
@@ -1,14 +1,18 @@
-package BsicSyntaxDataType;
+package BasicSyntaxDataType;
 //created by J.M.
+
 import java.util.Scanner;
 
 public class DecryptingMessage {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
-        int key=Integer.parseInt(scan.nextLine());
-        int n=Integer.parseInt(scan.nextLine());
-        for (int i=1; i<=n; i++){
-            System.out.printf("%c",(scan.nextLine().charAt(0) + key));
+        int key = Integer.parseInt(scan.nextLine());
+        int n = Integer.parseInt(scan.nextLine());
+
+        for (int i = 1; i <= n; i++) {
+            System.out.printf("%c", (scan.nextLine().charAt(0) + key));
         }
     }
 }
