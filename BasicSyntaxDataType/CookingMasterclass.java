7f206b57c8f82cddf6499542570a9ca9641ede2a
Petrov-connect <Petrov.connect@gmail.com>
2021-01-08 16:01:28 +0200

    java code

--- BasicSyntaxDataType/CookingMasterclass.java
+++ BasicSyntaxDataType/CookingMasterclass.java
@@ -1,9 +1,12 @@
-package BsicSyntaxDataType;
+package BasicSyntaxDataType;
 //created by J.M.
+
 import java.util.*;
 
 public class CookingMasterclass {
+
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
         double budget = Double.parseDouble(scanner.nextLine());
         int students = Integer.parseInt(scanner.nextLine());
@@ -12,11 +15,11 @@ public class CookingMasterclass {
         double apronPrice = Double.parseDouble(scanner.nextLine());
 
         double total = (apronPrice * (Math.ceil(students * 1.2))) + (eggPrice * 10 * students)
-                + (flourPrice * (students - (Math.floor((double)students / 5))));
-        if (total <= budget){
+                + (flourPrice * (students - (Math.floor((double) students / 5))));
+
+        if (total <= budget) {
             System.out.printf("Items purchased for %.02f$.", total);
-        }
-        else{
+        } else {
             System.out.printf("%.02f$ more needed.", total - budget);
         }
     }
