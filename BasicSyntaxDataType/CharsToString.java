33f32c8fa8971f5abbd93ee5d5047454325ee2fd
Petrov-connect <Petrov.connect@gmail.com>
2021-01-08 16:00:58 +0200

    java code

--- BasicSyntaxDataType/CharsToString.java
+++ BasicSyntaxDataType/CharsToString.java
@@ -1,13 +1,17 @@
-package BsicSyntaxDataType;
+package BasicSyntaxDataType;
 //created by J.M.
+
 import java.util.Scanner;
 
 public class CharsToString {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
         char one = scan.nextLine().charAt(0);
         char two = scan.nextLine().charAt(0);
         char three = scan.nextLine().charAt(0);
-        System.out.printf("%c%c%c",one,two,three);
+
+        System.out.printf("%c%c%c", one, two, three);
     }
 }
