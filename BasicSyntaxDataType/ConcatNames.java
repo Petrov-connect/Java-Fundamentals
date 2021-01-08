4dbf1d83fb9eee1f637422d0f6fdb98efe248654
Petrov-connect <Petrov.connect@gmail.com>
2021-01-08 16:01:08 +0200

    java code

--- BasicSyntaxDataType/ConcatNames.java
+++ BasicSyntaxDataType/ConcatNames.java
@@ -1,13 +1,16 @@
-package BsicSyntaxDataType;
+package BasicSyntaxDataType;
 //created by J.M.
 import java.util.Scanner;
 
 public class ConcatNames {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
         String name1=scan.nextLine();
         String name2=scan.nextLine();
         String delimiter=scan.nextLine();
+
         System.out.printf("%s%s%s",name1,delimiter,name2);
     }
 }
