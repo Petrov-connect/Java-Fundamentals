5f3a278ca223f0072c2ca0830235736d5b1d7fa2
Petrov-connect <Petrov.connect@gmail.com>
2021-01-08 16:01:18 +0200

    java code

--- BasicSyntaxDataType/ConvertMetersToKilometers.java
+++ BasicSyntaxDataType/ConvertMetersToKilometers.java
@@ -1,11 +1,15 @@
-package BsicSyntaxDataType;
+package BasicSyntaxDataType;
 //created by J.M.
+
 import java.util.Scanner;
 
 public class ConvertMetersToKilometers {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
-        int meters=Integer.parseInt(scan.nextLine());
-        System.out.printf("%.2f",(double) meters/1000);
+        int meters = Integer.parseInt(scan.nextLine());
+
+        System.out.printf("%.2f", (double) meters / 1000);
     }
 }
