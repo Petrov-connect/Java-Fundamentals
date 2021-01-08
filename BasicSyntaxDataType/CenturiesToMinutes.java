4dac5af04b405b93e6a5ae3aaf3035ceededb9c4
Petrov-connect <Petrov.connect@gmail.com>
2021-01-08 16:00:47 +0200

    java code

--- BasicSyntaxDataType/CenturiesToMinutes.java
+++ BasicSyntaxDataType/CenturiesToMinutes.java
@@ -1,15 +1,20 @@
-package BsicSyntaxDataType;
+package BasicSyntaxDataType;
 //created by J.M.
+
 import java.util.Scanner;
 
 public class CenturiesToMinutes {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
-        int centuries=Integer.parseInt(scan.nextLine());
-        int years=centuries*100;
-        int days= (int) (years*365.2422);
-        int hours=days*24;
-        long minutes=hours*60L;
-        System.out.printf("%d centuries = %d years = %d days = %d hours = %d minutes",centuries,years,days,hours,minutes);
+        int centuries = Integer.parseInt(scan.nextLine());
+
+        int years = centuries * 100;
+        int days = (int) (years * 365.2422);
+        int hours = days * 24;
+        long minutes = hours * 60L;
+
+        System.out.printf("%d centuries = %d years = %d days = %d hours = %d minutes", centuries, years, days, hours, minutes);
     }
 }
