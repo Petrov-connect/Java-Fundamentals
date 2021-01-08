2d7351c2e4120438b739ea1fe7b593764634891a
Petrov-connect <Petrov.connect@gmail.com>
2021-01-08 16:00:25 +0200

    java code

--- BasicSyntaxDataType/BalancedBrackets.java
+++ BasicSyntaxDataType/BalancedBrackets.java
@@ -1,29 +1,40 @@
-package BsicSyntaxDataType;
+package BasicSyntaxDataType;
 //created by J.M.
+
 import java.util.Scanner;
 
 public class BalancedBrackets {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
-        int n=Integer.parseInt(scan.nextLine());
-        boolean unbalanced=false; boolean first=false; int count1=0; int count2=0;
-        for(int i=1; i<=n; i++) {
+        int n = Integer.parseInt(scan.nextLine());
+
+        boolean unbalanced = false;
+        boolean first = false;
+        int count1 = 0;
+        int count2 = 0;
+
+        for (int i = 1; i <= n; i++) {
             String input = scan.nextLine();
-            if (((input.equals("("))&&first)) {
+            if (((input.equals("(")) && first)) {
                 unbalanced = true;
             } else {
-                if(input.equals("(")){
-                    count1++;first=true;
-                }else if(input.equals(")")){
-                    count2++;first=false;
+                if (input.equals("(")) {
+                    count1++;
+                    first = true;
+                } else if (input.equals(")")) {
+                    count2++;
+                    first = false;
                 }
-            } if((first&&i==n)){
-                unbalanced=true;
+            }
+            if ((first && i == n)) {
+                unbalanced = true;
             }
         }
-        if(!unbalanced&&count1==count2){
+        if (!unbalanced && count1 == count2) {
             System.out.println("BALANCED");
-        }else{
+        } else {
             System.out.println("UNBALANCED");
         }
     }
