35d4f9b43f83398e91289b7f4233b5410f39113c
Petrov-connect <Petrov.connect@gmail.com>
2021-01-09 16:04:33 +0200

    java code

--- BasicSyntaxDataType/FloatingEquality.java
+++ BasicSyntaxDataType/FloatingEquality.java
@@ -1,15 +1,19 @@
-package BsicSyntaxDataType;
+package BasicSyntaxDataType;
 //created by J.M.
+
 import java.util.Scanner;
 
 public class FloatingEquality {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
-        double a=Double.parseDouble(scan.nextLine());
-        double b=Double.parseDouble(scan.nextLine());
-        if(Math.abs(a-b)<0.000001){
+        double a = Double.parseDouble(scan.nextLine());
+        double b = Double.parseDouble(scan.nextLine());
+
+        if (Math.abs(a - b) < 0.000001) {
             System.out.println("True");
-        }else{
+        } else {
             System.out.println("False");
         }
     }
