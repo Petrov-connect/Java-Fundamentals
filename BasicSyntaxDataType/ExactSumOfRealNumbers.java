eb5ddd22cca92695b589b17ec809ef80f0dd3441
Petrov-connect <Petrov.connect@gmail.com>
2021-01-09 16:04:23 +0200

    java code

--- BasicSyntaxDataType/ExactSumOfRealNumbers.java
+++ BasicSyntaxDataType/ExactSumOfRealNumbers.java
@@ -1,14 +1,19 @@
-package BsicSyntaxDataType;
+package BasicSyntaxDataType;
 //created by J.M.
+
 import java.math.BigDecimal;
 import java.util.Scanner;
 
 public class ExactSumOfRealNumbers {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
         int n = Integer.parseInt(scan.nextLine());
-        BigDecimal sum=new BigDecimal(0);
-        for(int i=1; i<=n; i++){
+
+        BigDecimal sum = new BigDecimal(0);
+
+        for (int i = 1; i <= n; i++) {
             BigDecimal num = new BigDecimal(scan.nextLine());
             sum = sum.add(num);
         }
