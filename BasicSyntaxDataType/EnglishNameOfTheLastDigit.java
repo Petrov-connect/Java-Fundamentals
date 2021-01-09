54597f3d736f4fb1118886741208ea4374fa7cba
Petrov-connect <Petrov.connect@gmail.com>
2021-01-09 16:04:02 +0200

    java code

--- BasicSyntaxDataType/EnglishNameOfTheLastDigit.java
+++ BasicSyntaxDataType/EnglishNameOfTheLastDigit.java
@@ -1,12 +1,17 @@
-package BsicSyntaxDataType;
+package BasicSyntaxDataType;
 //created by J.M.
+
 import java.util.Scanner;
 
 public class EnglishNameOfTheLastDigit {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
-        int num=Integer.parseInt(scan.nextLine());
+        int num = Integer.parseInt(scan.nextLine());
+
         String[] name = new String[]{"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
-        System.out.println(name[num%10]);
+
+        System.out.println(name[num % 10]);
     }
 }
