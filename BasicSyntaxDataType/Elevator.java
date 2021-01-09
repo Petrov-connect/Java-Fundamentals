ec7842052e172c1365bab4f9b05e36f5cb4b3b39
Petrov-connect <Petrov.connect@gmail.com>
2021-01-09 16:03:52 +0200

    java code

--- BasicSyntaxDataType/Elevator.java
+++ BasicSyntaxDataType/Elevator.java
@@ -1,13 +1,17 @@
-package BsicSyntaxDataType;
+package BasicSyntaxDataType;
 //created by J.M.
+
 import java.util.Scanner;
 
 public class Elevator {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
-        int people=Integer.parseInt(scan.nextLine());
-        int capacity=Integer.parseInt(scan.nextLine());
-        int courses=(int) Math.ceil((double) people/capacity);
+        int people = Integer.parseInt(scan.nextLine());
+        int capacity = Integer.parseInt(scan.nextLine());
+        int courses = (int) Math.ceil((double) people / capacity);
+
         System.out.println(courses);
     }
 }
