bf78105dbc17235322fae756afe375df5eb02ab3
Petrov-connect <Petrov.connect@gmail.com>
2021-01-09 16:04:13 +0200

    java code

--- BasicSyntaxDataType/EvenNumber.java
+++ BasicSyntaxDataType/EvenNumber.java
@@ -1,11 +1,15 @@
-package BsicSyntaxDataType;
+package BasicSyntaxDataType;
 //created by J.M.
+
 import java.util.Scanner;
 
 public class EvenNumber {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
         int num = Integer.parseInt(scan.nextLine());
+
         while (num % 2 != 0) {
             System.out.println("Please write an even number.");
             num = Integer.parseInt(scan.nextLine());
