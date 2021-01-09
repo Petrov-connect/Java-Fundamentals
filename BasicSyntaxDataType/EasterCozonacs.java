7a738598de452411c13f8696bf7a331222afa74c
Petrov-connect <Petrov.connect@gmail.com>
2021-01-09 16:03:41 +0200

    java code

--- BasicSyntaxDataType/EasterCozonacs.java
+++ BasicSyntaxDataType/EasterCozonacs.java
@@ -1,24 +1,30 @@
-package BsicSyntaxDataType;
+package BasicSyntaxDataType;
 //created by J.M.
+
 import java.util.*;
 
 public class EasterCozonacs {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
-        double budget=Double.parseDouble(scan.nextLine());
-        double priceFlour=Double.parseDouble(scan.nextLine());
-        double priceEgg=priceFlour*0.75;
-        double priceMilk=priceFlour*1.25;
-        double priceForOne=priceEgg+priceFlour+priceMilk/4;
-        int cozonacs=0; int colorEggs=0;
-        while (budget>=priceForOne){
+        double budget = Double.parseDouble(scan.nextLine());
+        double priceFlour = Double.parseDouble(scan.nextLine());
+
+        double priceEgg = priceFlour * 0.75;
+        double priceMilk = priceFlour * 1.25;
+        double priceForOne = priceEgg + priceFlour + priceMilk / 4;
+        int cozonacs = 0;
+        int colorEggs = 0;
+
+        while (budget >= priceForOne) {
             cozonacs++;
-            colorEggs+=3;
-            if(cozonacs%3==0){
-                colorEggs-=cozonacs-2;
+            colorEggs += 3;
+            if (cozonacs % 3 == 0) {
+                colorEggs -= cozonacs - 2;
             }
-            budget-=priceForOne;
+            budget -= priceForOne;
         }
-        System.out.printf("You made %d cozonacs! Now you have %d eggs and %.2fBGN left.",cozonacs,colorEggs,budget);
+        System.out.printf("You made %d cozonacs! Now you have %d eggs and %.2fBGN left.", cozonacs, colorEggs, budget);
     }
 }
