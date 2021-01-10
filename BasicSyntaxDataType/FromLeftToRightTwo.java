786a5b82a3797397c6fb532bcc23ed5ceb47813e
Petrov-connect <Petrov.connect@gmail.com>
2021-01-10 19:01:43 +0200

    java code

--- BasicSyntaxDataType/FromLeftToRightTwo.java
+++ BasicSyntaxDataType/FromLeftToRightTwo.java
@@ -1,11 +1,15 @@
-package BsicSyntaxDataType;
+package BasicSyntaxDataType;
 //created by J.M.
+
 import java.util.Scanner;
 
-public class FromLefrToRight2 {
+public class FromLeftToRightTwo {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
         int n = Integer.parseInt(scan.nextLine());
+
         for (int i = 1; i <= n; i++) {
             String input = scan.nextLine();
             String[] parts = input.split(" ");
