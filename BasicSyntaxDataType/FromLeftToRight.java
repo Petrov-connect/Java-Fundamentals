0e9f85dafe92f4c53dec8f5d9abca4e5388113ed
Petrov-connect <Petrov.connect@gmail.com>
2021-01-10 19:01:28 +0200

    java code

--- BasicSyntaxDataType/FromLeftToRight.java
+++ BasicSyntaxDataType/FromLeftToRight.java
@@ -1,21 +1,25 @@
-package BsicSyntaxDataType;
+package BasicSyntaxDataType;
 //created by J.M.
+
 import java.util.Scanner;
 
-public class FromLeftToRight1 {
+public class FromLeftToRight {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
         int n = Integer.parseInt(scan.nextLine());
+
         for (int i = 1; i <= n; i++) {
             long left = scan.nextLong();
             long right = scan.nextLong();
             int sum = 0;
             long biggest = Math.max(left, right);
-                while (biggest != 0) {
-                    sum += biggest % 10;
-                    biggest /= 10;
-                }
-                System.out.println(Math.abs(sum));
+            while (biggest != 0) {
+                sum += biggest % 10;
+                biggest /= 10;
+            }
+            System.out.println(Math.abs(sum));
         }
     }
 }
