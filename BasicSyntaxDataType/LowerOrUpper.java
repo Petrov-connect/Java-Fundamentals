4f65b6f466bd2b8816d937b6da1f0eccdf94a551
Petrov-connect <Petrov.connect@gmail.com>
2021-01-10 19:02:23 +0200

    java code

--- BasicSyntaxDataType/LowerOrUpper.java
+++ BasicSyntaxDataType/LowerOrUpper.java
@@ -1,14 +1,18 @@
-package BsicSyntaxDataType;
+package BasicSyntaxDataType;
 //created by J.M.
+
 import java.util.Scanner;
 
 public class LowerOrUpper {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
-        char input=scan.nextLine().charAt(0);
-        if(input>64&&input<91){
+        char input = scan.nextLine().charAt(0);
+
+        if (input > 64 && input < 91) {
             System.out.println("upper-case");
-        }else{
+        } else {
             System.out.println("lower-case");
         }
     }
