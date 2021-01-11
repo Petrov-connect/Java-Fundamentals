20aec43875a0eebb369f92d8572067080fe625d6
Petrov-connect <Petrov.connect@gmail.com>
2021-01-11 17:39:28 +0200

    java code

--- BasicSyntaxDataType/PoundsToDollars.java
+++ BasicSyntaxDataType/PoundsToDollars.java
@@ -1,11 +1,15 @@
-package BsicSyntaxDataType;
+package BasicSyntaxDataType;
 //created by J.M.
+
 import java.util.Scanner;
 
 public class PoundsToDollars {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
         double pounds = scan.nextDouble();
-        System.out.printf("%.3f",pounds*1.31);
+
+        System.out.printf("%.3f", pounds * 1.31);
     }
 }
