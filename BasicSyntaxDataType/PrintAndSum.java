ade54a46513528638da6c1dd17b435f0c9ae2408
Petrov-connect <Petrov.connect@gmail.com>
2021-01-11 17:39:37 +0200

    java code

--- BasicSyntaxDataType/PrintAndSum.java
+++ BasicSyntaxDataType/PrintAndSum.java
@@ -1,18 +1,23 @@
-package BsicSyntaxDataType;
+package BasicSyntaxDataType;
 //created by J.M.
+
 import java.util.Scanner;
 
 public class PrintAndSum {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
-        int start=Integer.parseInt(scan.nextLine());
-        int end=Integer.parseInt(scan.nextLine());
-        int sum=0;
-        for(int i=start; i<=end; i++){
-            System.out.print(i+" ");
-            sum+=i;
+        int start = Integer.parseInt(scan.nextLine());
+        int end = Integer.parseInt(scan.nextLine());
+
+        int sum = 0;
+
+        for (int i = start; i <= end; i++) {
+            System.out.print(i + " ");
+            sum += i;
         }
         System.out.println();
-        System.out.println("Sum: "+sum);
+        System.out.println("Sum: " + sum);
     }
 }
