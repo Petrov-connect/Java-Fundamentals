b3d03c36f1ddf17fd8c6208cc7fd0926d64d5a2f
Petrov-connect <Petrov.connect@gmail.com>
2021-01-11 17:38:58 +0200

    java code

--- BasicSyntaxDataType/Passed.java
+++ BasicSyntaxDataType/Passed.java
@@ -1,12 +1,16 @@
-package BsicSyntaxDataType;
+package BasicSyntaxDataType;
 //created by J.M.
+
 import java.util.Scanner;
 
 public class Passed {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
         double grade = Double.parseDouble(scan.nextLine());
-        if (grade>=3.00){
+
+        if (grade >= 3.00) {
             System.out.println("BasicSyntax.Passed!");
         }
     }
