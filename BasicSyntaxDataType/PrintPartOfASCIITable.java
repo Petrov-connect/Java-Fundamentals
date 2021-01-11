e6dbbe08e4d7f5d9bb634f74f30aacf6df11a9f5
Petrov-connect <Petrov.connect@gmail.com>
2021-01-11 17:39:47 +0200

    java code

--- BasicSyntaxDataType/PrintPartOfASCIITable.java
+++ BasicSyntaxDataType/PrintPartOfASCIITable.java
@@ -1,14 +1,18 @@
-package BsicSyntaxDataType;
+package BasicSyntaxDataType;
 //created by J.M.
+
 import java.util.Scanner;
 
 public class PrintPartOfASCIITable {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
-        int start=Integer.parseInt(scan.nextLine());
-        int end=Integer.parseInt(scan.nextLine());
-        for(char a=(char )start; a<=end; a++ ){
-            System.out.printf("%c ",a);
+        int start = Integer.parseInt(scan.nextLine());
+        int end = Integer.parseInt(scan.nextLine());
+
+        for (char a = (char) start; a <= end; a++) {
+            System.out.printf("%c ", a);
         }
     }
 }
