2e9d73c8d219bd3382fd87a857dce7c5e6a215b3
Petrov-connect <Petrov.connect@gmail.com>
2021-01-11 17:40:07 +0200

    java code

--- BasicSyntaxDataType/RefactoringPrimeChecker.java
+++ BasicSyntaxDataType/RefactoringPrimeChecker.java
@@ -1,11 +1,14 @@
-package BsicSyntaxDataType;
+package BasicSyntaxDataType;
 //created by J.M.
 import java.util.Scanner;
 
 public class RefactoringPrimeChecker {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
         int digit=Integer.parseInt(scan.nextLine());
+
         for(int i=2;i <=digit; i++){
             boolean prime= true; int count=0;
             for(int j=2; j<=digit; j++){
