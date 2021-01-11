b95ab8e933d4c7551dd88ef16fa6470c19482872
Petrov-connect <Petrov.connect@gmail.com>
2021-01-11 17:38:26 +0200

    java code

--- BasicSyntaxDataType/MultiplicationTableTwo.java
+++ BasicSyntaxDataType/MultiplicationTableTwo.java
@@ -1,15 +1,19 @@
-package BsicSyntaxDataType;
+package BasicSyntaxDataType;
 //created by J.M.
+
 import java.util.Scanner;
 
-public class MultiplicationTable2 {
+public class MultiplicationTableTwo {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
-        int n=Integer.parseInt(scan.nextLine());
-        int num=Integer.parseInt(scan.nextLine());
-        if(num>10){
+        int n = Integer.parseInt(scan.nextLine());
+        int num = Integer.parseInt(scan.nextLine());
+
+        if (num > 10) {
             System.out.println(n + " X " + num + " = " + n * num);
-        }else {
+        } else {
             for (int i = num; i <= 10; i++) {
                 System.out.println(n + " X " + i + " = " + n * i);
             }
