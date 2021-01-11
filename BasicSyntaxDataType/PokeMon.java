84d2016b7107a3ba3ce340b078297d4b88e6f70f
Petrov-connect <Petrov.connect@gmail.com>
2021-01-11 17:39:18 +0200

    java code

--- BasicSyntaxDataType/PokeMon.java
+++ BasicSyntaxDataType/PokeMon.java
@@ -1,17 +1,27 @@
-package BsicSyntaxDataType;
+package BasicSyntaxDataType;
 //created by J.M.
+
 import java.util.Scanner;
 
 public class PokeMon {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
-        int N=Integer.parseInt(scan.nextLine());
-        int M=Integer.parseInt(scan.nextLine());
-        int Y=Integer.parseInt(scan.nextLine());
-        int count = 0; int x = N/2; boolean realHalf = N%x==0;
+        int N = Integer.parseInt(scan.nextLine());
+        int M = Integer.parseInt(scan.nextLine());
+        int Y = Integer.parseInt(scan.nextLine());
+
+        int count = 0;
+        int x = N / 2;
+        boolean realHalf = N % x == 0;
+
         while (N >= M) {
-            N -= M; count++;
-            if (Y>0 && realHalf && N==x) { N /= Y; }
+            N -= M;
+            count++;
+            if (Y > 0 && realHalf && N == x) {
+                N /= Y;
+            }
         }
         System.out.println(N);
         System.out.println(count);
