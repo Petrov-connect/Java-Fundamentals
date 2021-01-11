2383e087ef49e6051eb96d8c9e487ca775c818a5
Petrov-connect <Petrov.connect@gmail.com>
2021-01-11 17:38:14 +0200

    java code

--- BasicSyntaxDataType/MultiplicationTable.java
+++ BasicSyntaxDataType/MultiplicationTable.java
@@ -1,13 +1,17 @@
-package BsicSyntaxDataType;
+package BasicSyntaxDataType;
 //created by J.M.
+
 import java.util.Scanner;
 
 public class MultiplicationTable {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
-        int n=Integer.parseInt(scan.nextLine());
-        for(int i=1; i<=10; i++){
-            System.out.println(n+" X "+i+" = "+n*i);
+        int n = Integer.parseInt(scan.nextLine());
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(n + " X " + i + " = " + n * i);
         }
     }
 }
