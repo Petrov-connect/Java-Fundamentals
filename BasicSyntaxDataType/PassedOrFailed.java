8e313ea749ea5df5bf4f20b9f4ba37eee3f9edc2
Petrov-connect <Petrov.connect@gmail.com>
2021-01-11 17:39:08 +0200

    java code

--- BasicSyntaxDataType/PassedOrFailed.java
+++ BasicSyntaxDataType/PassedOrFailed.java
@@ -1,14 +1,18 @@
-package BsicSyntaxDataType;
+package BasicSyntaxDataType;
 //created by J.M.
+
 import java.util.Scanner;
 
 public class PassedOrFailed {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
         double grade = Double.parseDouble(scan.nextLine());
+
         if (grade > 2.99) {
             System.out.println("BasicSyntax.Passed!");
-        }else {
+        } else {
             System.out.println("Failed!");
         }
     }
