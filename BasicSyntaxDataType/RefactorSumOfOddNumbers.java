7ea72b426fd63e29b39416b550cdde32ce663e73
Petrov-connect <Petrov.connect@gmail.com>
2021-01-12 15:55:10 +0200

    java code

--- BasicSyntaxDataType/RefactorSumOfOddNumbers.java
+++ BasicSyntaxDataType/RefactorSumOfOddNumbers.java
@@ -1,16 +1,21 @@
-package BsicSyntaxDataType;
+package BasicSyntaxDataType;
 //created by J.M.
+
 import java.util.Scanner;
 
 public class RefactorSumOfOddNumbers {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
         int n = Integer.parseInt(scan.nextLine());
-        int sum=0;
-        for(int i=0; i<n; i++){
-            System.out.println(2*i+1);
-            sum+=2*i+1;
+
+        int sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            System.out.println(2 * i + 1);
+            sum += 2 * i + 1;
         }
-        System.out.printf("Sum: %d%n",sum);
+        System.out.printf("Sum: %d%n", sum);
     }
 }
