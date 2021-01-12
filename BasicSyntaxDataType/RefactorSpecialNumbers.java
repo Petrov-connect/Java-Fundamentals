512cdf44dcc803d84943c01759b30d15b736f812
Petrov-connect <Petrov.connect@gmail.com>
2021-01-12 15:54:56 +0200

    java code

--- BasicSyntaxDataType/RefactorSpecialNumbers.java
+++ BasicSyntaxDataType/RefactorSpecialNumbers.java
@@ -1,15 +1,21 @@
-package BsicSyntaxDataType;
+package BasicSyntaxDataType;
 //created by J.M.
+
 import java.util.Scanner;
 
 public class RefactorSpecialNumbers {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
         int num = Integer.parseInt(scan.nextLine());
+
         for (int i = 1; i <= num; i++) {
-            int sum = 0; int actualNum = i;
+            int sum = 0;
+            int actualNum = i;
             while (i > 0) {
-                sum += i % 10; i = i / 10;
+                sum += i % 10;
+                i = i / 10;
             }
             boolean trueFalse = (sum == 5) || (sum == 7) || (sum == 11);
             String tF;
