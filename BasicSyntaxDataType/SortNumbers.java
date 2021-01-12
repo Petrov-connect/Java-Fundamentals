98f480f2786199f4882ab7897d56b10b622bdb12
Petrov-connect <Petrov.connect@gmail.com>
2021-01-12 15:56:29 +0200

    java code

--- BasicSyntaxDataType/SortNumbers.java
+++ BasicSyntaxDataType/SortNumbers.java
@@ -1,25 +1,34 @@
-package BsicSyntaxDataType;
+package BasicSyntaxDataType;
 //created by J.M.
+
 import java.util.Scanner;
 
 public class SortNumbers {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
-        int num1=Integer.parseInt(scan.nextLine());
-        int num2=Integer.parseInt(scan.nextLine());
-        int num3=Integer.parseInt(scan.nextLine());
-        int max;int min;int mid;
-        if(num1>num2){
-            max=num1;min=num2;
-        }else{
-            max=num2;min=num1;
+        int num1 = Integer.parseInt(scan.nextLine());
+        int num2 = Integer.parseInt(scan.nextLine());
+        int num3 = Integer.parseInt(scan.nextLine());
+
+        int max,min,mid;
+
+        if (num1 > num2) {
+            max = num1;
+            min = num2;
+        } else {
+            max = num2;
+            min = num1;
         }
-        if(num3>max){
-            mid=max;max=num3;
-        }else if(num3<min){
-            mid=min;min=num3;
-        }else{
-            mid=num3;
+        if (num3 > max) {
+            mid = max;
+            max = num3;
+        } else if (num3 < min) {
+            mid = min;
+            min = num3;
+        } else {
+            mid = num3;
         }
         System.out.println(max);
         System.out.println(mid);
