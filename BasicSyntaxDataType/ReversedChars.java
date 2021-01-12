5091d79de9ddf3bfe94f9a12a6554e615e5ad529
Petrov-connect <Petrov.connect@gmail.com>
2021-01-12 15:55:46 +0200

    java code

--- BasicSyntaxDataType/ReversedChars.java
+++ BasicSyntaxDataType/ReversedChars.java
@@ -1,13 +1,17 @@
-package BsicSyntaxDataType;
+package BasicSyntaxDataType;
 //created by J.M.
+
 import java.util.Scanner;
 
 public class ReversedChars {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
         char one = scan.nextLine().charAt(0);
         char two = scan.nextLine().charAt(0);
         char three = scan.nextLine().charAt(0);
-        System.out.printf("%c %c %c",three,two,one);
+
+        System.out.printf("%c %c %c", three, two, one);
     }
 }
