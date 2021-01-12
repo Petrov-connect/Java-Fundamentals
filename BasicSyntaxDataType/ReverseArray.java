17e21e9f0dccd21d73f707f7b57fd2dd58b69131
Petrov-connect <Petrov.connect@gmail.com>
2021-01-12 15:55:35 +0200

    java code

--- BasicSyntaxDataType/ReverseArray.java
+++ BasicSyntaxDataType/ReverseArray.java
@@ -1,22 +1,26 @@
-package BsicSyntaxDataType;
+package BasicSyntaxDataType;
 //created by J.M.
+
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class ReverseArray {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
-        int n=Integer.parseInt(scan.nextLine());
-        int[] numbers=new int[n];
-        for(int i=0; i<numbers.length; i++){
-            numbers[i]=scan.nextInt();
+        int n = Integer.parseInt(scan.nextLine());
+
+        int[] numbers = new int[n];
+
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = scan.nextInt();
         }
-        for(int i=0; i<numbers.length/2; i++){
-            int oldNum=numbers[i];
-            numbers[i]=numbers[numbers.length-1-i];
-            numbers[numbers.length-1-i]=oldNum;
+        for (int i = 0; i < numbers.length / 2; i++) {
+            int oldNum = numbers[i];
+            numbers[i] = numbers[numbers.length - 1 - i];
+            numbers[numbers.length - 1 - i] = oldNum;
         }
-        for (int number : numbers) {
-            System.out.print(number+" ");
-        }
+        Arrays.stream(numbers).mapToObj(number -> number + " ").forEach(System.out::print);
     }
 }
