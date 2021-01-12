c2a0f5b10eb9f990433d197d787c2423d03faa90
Petrov-connect <Petrov.connect@gmail.com>
2021-01-12 15:55:58 +0200

    java code

--- BasicSyntaxDataType/ReverseStrings.java
+++ BasicSyntaxDataType/ReverseStrings.java
@@ -1,13 +1,17 @@
-package BsicSyntaxDataType;
+package BasicSyntaxDataType;
 //created by J.M.
+
 import java.util.*;
 
 public class ReverseStrings {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
         String input;
-        while (!"end".equals(input=scan.nextLine())){
-            System.out.printf("%s = %s%n",input,new StringBuilder(input).reverse());
+
+        while (!"end".equals(input = scan.nextLine())) {
+            System.out.printf("%s = %s%n", input, new StringBuilder(input).reverse());
         }
     }
 }
