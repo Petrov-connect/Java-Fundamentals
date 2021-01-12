93f38555c31255437b5e2736c3ee72d1c8babbc0
Petrov-connect <Petrov.connect@gmail.com>
2021-01-12 15:56:09 +0200

    java code

--- BasicSyntaxDataType/Snowballs.java
+++ BasicSyntaxDataType/Snowballs.java
@@ -1,22 +1,32 @@
-package BsicSyntaxDataType;
+package BasicSyntaxDataType;
 //created by J.M.
+
 import java.util.Scanner;
 
 public class Snowballs {
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
-        int n=Integer.parseInt(scan.nextLine());
-        long snowballValue=0; int snowballSnow=0;int snowballTime=0; int snowballQuality=0;
-        for(int i=1; i<=n; i++){
-            int newSnowballSnow=Integer.parseInt(scan.nextLine());
-            int newSnowballTime=Integer.parseInt(scan.nextLine());
-            int newSnowballQuality=Integer.parseInt(scan.nextLine());
-            long sum = (long) Math.pow(((double)newSnowballSnow/newSnowballTime), newSnowballQuality);
-            if(sum>snowballValue){
-                snowballValue=sum;snowballSnow=newSnowballSnow;snowballTime=newSnowballTime;snowballQuality=newSnowballQuality;
+        int n = Integer.parseInt(scan.nextLine());
+
+        long snowballValue = 0;
+        int snowballSnow = 0;
+        int snowballTime = 0;
+        int snowballQuality = 0;
+
+        for (int i = 1; i <= n; i++) {
+            int newSnowballSnow = Integer.parseInt(scan.nextLine());
+            int newSnowballTime = Integer.parseInt(scan.nextLine());
+            int newSnowballQuality = Integer.parseInt(scan.nextLine());
+            long sum = (long) Math.pow(((double) newSnowballSnow / newSnowballTime), newSnowballQuality);
+            if (sum > snowballValue) {
+                snowballValue = sum;
+                snowballSnow = newSnowballSnow;
+                snowballTime = newSnowballTime;
+                snowballQuality = newSnowballQuality;
             }
         }
-        System.out.printf("%d : %d = %d (%d)",snowballSnow,snowballTime,snowballValue,snowballQuality);
+        System.out.printf("%d : %d = %d (%d)", snowballSnow, snowballTime, snowballValue, snowballQuality);
     }
 }
 
