f66b4a1adedbd98ca6fef3739c22a29cd835a3ea
Petrov-connect <Petrov.connect@gmail.com>
2021-01-12 15:55:21 +0200

    java code

--- BasicSyntaxDataType/RefactorVolumeOfPyramid.java
+++ BasicSyntaxDataType/RefactorVolumeOfPyramid.java
@@ -1,17 +1,22 @@
-package BsicSyntaxDataType;
+package BasicSyntaxDataType;
 //created by J.M.
+
 import java.util.Scanner;
 
 public class RefactorVolumeOfPyramid {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
         System.out.print("Length: ");
         double length = Double.parseDouble(scan.nextLine());
         System.out.print("Width: ");
         double width = Double.parseDouble(scan.nextLine());
         System.out.print("Height: ");
-        double height=Double.parseDouble(scan.nextLine());
-        double volume =(length*width*height)/3;
-        System.out.printf("Pyramid Volume: %.2f",volume);
+        double height = Double.parseDouble(scan.nextLine());
+
+        double volume = (length * width * height) / 3;
+
+        System.out.printf("Pyramid Volume: %.2f", volume);
     }
 }
