f349bd98ae18715dfcf46d7988dc742e2fef8fcf
Petrov-connect <Petrov.connect@gmail.com>
2021-01-13 19:41:26 +0200

    java code

--- BasicSyntaxDataType/StudentInformation.java
+++ BasicSyntaxDataType/StudentInformation.java
@@ -1,13 +1,17 @@
-package BsicSyntaxDataType;
+package BasicSyntaxDataType;
 //created by J.M.
+
 import java.util.Scanner;
 
 public class StudentInformation {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
         String name = scan.nextLine();
         int age = Integer.parseInt(scan.nextLine());
         double averageGrade = Double.parseDouble(scan.nextLine());
-        System.out.printf("Name: %s, Age: %d, Grade: %.2f",name,age,averageGrade);
+
+        System.out.printf("Name: %s, Age: %d, Grade: %.2f", name, age, averageGrade);
     }
 }
