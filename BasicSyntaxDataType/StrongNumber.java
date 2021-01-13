669ca64900cc82bef81e57c2bd6146624e671088
Petrov-connect <Petrov.connect@gmail.com>
2021-01-13 19:41:16 +0200

    java code

--- BasicSyntaxDataType/StrongNumber.java
+++ BasicSyntaxDataType/StrongNumber.java
@@ -1,17 +1,22 @@
-package BsicSyntaxDataType;
+package BasicSyntaxDataType;
 //created by J.M.
+
 import java.util.Scanner;
 
 public class StrongNumber {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
         String num = scan.nextLine();
         int number = Integer.parseInt(num);
+
         int totalSum = 0;
+
         for (int i = 0; i < num.length(); i++) {
             int n = Integer.parseInt(String.valueOf((num).charAt(i)));
             int sum = 1;
-            for (int j = 1; j <=n; j++) {
+            for (int j = 1; j <= n; j++) {
                 sum *= j;
             }
             totalSum += sum;
