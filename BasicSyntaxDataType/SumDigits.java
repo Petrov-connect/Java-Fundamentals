958ab82c9d5b2cedb9517cc13fd20fe47295945a
Petrov-connect <Petrov.connect@gmail.com>
2021-01-13 19:41:37 +0200

    java code

--- BasicSyntaxDataType/SumDigits.java
+++ BasicSyntaxDataType/SumDigits.java
@@ -1,14 +1,19 @@
-package BsicSyntaxDataType;
+package BasicSyntaxDataType;
 //created by J.M.
+
 import java.util.Scanner;
 
 public class SumDigits {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
-        String num=scan.nextLine();
-        int sum=0;
-        for(int i=0; i<num.length(); i++){
-           sum+=Integer.parseInt(String.valueOf((num).charAt(i)));
+        String num = scan.nextLine();
+
+        int sum = 0;
+
+        for (int i = 0; i < num.length(); i++) {
+            sum += Integer.parseInt(String.valueOf((num).charAt(i)));
         }
         System.out.println(sum);
     }
