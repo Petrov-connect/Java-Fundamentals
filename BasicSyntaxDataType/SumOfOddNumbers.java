0f296672e65dc776c54b496a92cc0220409f5198
Petrov-connect <Petrov.connect@gmail.com>
2021-01-14 18:03:08 +0200

    java code

--- BasicSyntaxDataType/SumOfOddNumbers.java
+++ BasicSyntaxDataType/SumOfOddNumbers.java
@@ -1,16 +1,21 @@
-package BsicSyntaxDataType;
+package BasicSyntaxDataType;
 //created by J.M.
+
 import java.util.Scanner;
 
 public class SumOfOddNumbers {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
-        int n=Integer.parseInt(scan.nextLine());
-        int sum=0;
-        for (int i=1; i<=n*2; i+=2){
+        int n = Integer.parseInt(scan.nextLine());
+
+        int sum = 0;
+
+        for (int i = 1; i <= n * 2; i += 2) {
             System.out.println(i);
-            sum+=i;
+            sum += i;
         }
-        System.out.println("Sum: "+sum);
+        System.out.println("Sum: " + sum);
     }
 }
