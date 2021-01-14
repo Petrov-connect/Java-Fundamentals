e0d66dcf0a939718c6c032503b3dd558da7a39a3
Petrov-connect <Petrov.connect@gmail.com>
2021-01-14 18:04:32 +0200

    java code

--- BasicSyntaxDataType/WaterOverflow.java
+++ BasicSyntaxDataType/WaterOverflow.java
@@ -1,12 +1,16 @@
-package BsicSyntaxDataType;
+package BasicSyntaxDataType;
 //created by J.M.
 import java.util.Scanner;
 
 public class WaterOverflow {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
         int n=Integer.parseInt(scan.nextLine());
+
         int capacity=255;
+
         for(int i=1; i<=n; i++){
             int liters=Integer.parseInt(scan.nextLine());
             if(liters>capacity){
