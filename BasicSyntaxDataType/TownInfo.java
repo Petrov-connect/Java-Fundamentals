37b91685cb3608e7043cb05b14abb1a5df4ce397
Petrov-connect <Petrov.connect@gmail.com>
2021-01-14 18:03:41 +0200

    java code

--- BasicSyntaxDataType/TownInfo.java
+++ BasicSyntaxDataType/TownInfo.java
@@ -1,13 +1,17 @@
-package BsicSyntaxDataType;
+package BasicSyntaxDataType;
 //created by J.M.
+
 import java.util.Scanner;
 
 public class TownInfo {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
-        String town=scan.nextLine();
-        String population=scan.nextLine();
-        String area=scan.nextLine();
-        System.out.printf("Town %s has population of %s and area %s square km.",town,population,area);
+        String town = scan.nextLine();
+        String population = scan.nextLine();
+        String area = scan.nextLine();
+
+        System.out.printf("Town %s has population of %s and area %s square km.", town, population, area);
     }
 }
