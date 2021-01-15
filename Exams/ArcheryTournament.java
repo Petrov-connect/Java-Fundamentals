445cf9e3001b481bd414da7cc5ec8b51f374cd0d
Petrov-connect <Petrov.connect@gmail.com>
2021-01-15 22:34:09 +0200

    java code

--- Exams/ArcheryTournament.java
+++ Exams/ArcheryTournament.java
@@ -1,40 +1,48 @@
 package Exams;
 //created by J.M.
+
 import java.util.*;
 import java.util.stream.Collectors;
 
 public class ArcheryTournament {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
-        List<Integer> archeryField= Arrays.stream(scan.nextLine().split("\\|+")).map(Integer::parseInt).collect(Collectors.toList());
-        String input; int points=0;
-        while (!"Game over".equals(input=scan.nextLine())) {
+        List<Integer> archeryField = Arrays.stream(scan.nextLine().split("\\|+"))
+                .map(Integer::parseInt).collect(Collectors.toList());
+
+        String input;
+        int points = 0;
+
+        while (!"Game over".equals(input = scan.nextLine())) {
             String[] command = input.split("@");
-            if(command[0].equals("Reverse")){
+            if (command[0].equals("Reverse")) {
                 Collections.reverse(archeryField);
-            }else if (Integer.parseInt(command[1]) >= 0 && Integer.parseInt(command[1]) < archeryField.size()) {
+            } else if (Integer.parseInt(command[1]) >= 0 && Integer.parseInt(command[1]) < archeryField.size()) {
                 int shotIndex;
-                if(command[0].equals("Shoot Left")){
+                if (command[0].equals("Shoot Left")) {
                     shotIndex = Integer.parseInt(command[1]) - (Integer.parseInt(command[2]));
-                    while (shotIndex<0){
-                        shotIndex+=archeryField.size();
+                    while (shotIndex < 0) {
+                        shotIndex += archeryField.size();
                     }
-                }else{
+                } else {
                     shotIndex = Integer.parseInt(command[1]) + Integer.parseInt(command[2]);
-                    while (shotIndex>archeryField.size()-1){
-                        shotIndex-=archeryField.size();
+                    while (shotIndex > archeryField.size() - 1) {
+                        shotIndex -= archeryField.size();
                     }
                 }
-                if(archeryField.get(shotIndex)<5){
-                    points+=archeryField.get(shotIndex);
-                    archeryField.set(shotIndex,0);
-                }else{
-                    points+=5;
-                    archeryField.set(shotIndex,archeryField.get(shotIndex)-5);
+                if (archeryField.get(shotIndex) < 5) {
+                    points += archeryField.get(shotIndex);
+                    archeryField.set(shotIndex, 0);
+                } else {
+                    points += 5;
+                    archeryField.set(shotIndex, archeryField.get(shotIndex) - 5);
                 }
             }
         }
-        System.out.println(archeryField.toString().replaceAll("[\\[\\]]","").replaceAll(","," -"));
-        System.out.printf("Iskren finished the archery tournament with %d points!%n",points);
+        System.out.println(archeryField.toString().replaceAll("[\\[\\]]", "")
+                .replaceAll(",", " -"));
+        System.out.printf("Iskren finished the archery tournament with %d points!%n", points);
     }
 }
