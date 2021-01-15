5d795a23cca474e8bf840701fb97b2c3065cd52a
Petrov-connect <Petrov.connect@gmail.com>
2021-01-15 22:33:45 +0200

    java code

--- Exams/AdAstra.java
+++ Exams/AdAstra.java
@@ -1,22 +1,26 @@
 package Exams;
 //created by J.M.
+
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class AdAstra {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
-        String text=scan.nextLine();
-        List<String>output=new ArrayList<>();
-        Pattern pattern=Pattern.compile("([#|])(?<name>[A-Za-z ]+)\\1(?<date>\\d{2}/\\d{2}/\\d{2})\\1(?<calories>\\d+)\\1");
-        Matcher matcher=pattern.matcher(text);
-        int calories=0;
-        while (matcher.find()){
-            output.add(String.format("Item: %s, Best before: %s, Nutrition: %s",matcher.group("name"),matcher.group("date"),matcher.group("calories")));
-            calories+=Integer.parseInt(matcher.group("calories"));
+        String text = scan.nextLine();
+        List<String> output = new ArrayList<>();
+        Pattern pattern = Pattern.compile("([#|])(?<name>[A-Za-z ]+)\\1(?<date>\\d{2}/\\d{2}/\\d{2})\\1(?<calories>\\d+)\\1");
+        Matcher matcher = pattern.matcher(text);
+        int calories = 0;
+
+        while (matcher.find()) {
+            output.add(String.format("Item: %s, Best before: %s, Nutrition: %s", matcher.group("name"), matcher.group("date"), matcher.group("calories")));
+            calories += Integer.parseInt(matcher.group("calories"));
         }
-        System.out.printf("You have food to last you for: %d days!%n",calories/2000);
+        System.out.printf("You have food to last you for: %d days!%n", calories / 2000);
         output.forEach(System.out::println);
     }
 }
