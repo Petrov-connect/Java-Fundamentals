22426c6d5854f737b6091816565faa2fdab82107
Petrov-connect <Petrov.connect@gmail.com>
2021-01-15 22:34:21 +0200

    java code

--- Exams/Crafting.java
+++ Exams/Crafting.java
@@ -1,33 +1,37 @@
 package Exams;
 //created by J.M.
+
 import java.util.*;
 import java.util.stream.IntStream;
 
 public class Crafting {
+
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
         String[] weaponName = scanner.nextLine().split("\\|");
         String input;
+
         while (!"Done".equals(input = scanner.nextLine())) {
             String[] command = input.split("\\s+");
             switch (command[0]) {
                 case "Move":
                     int index = Integer.parseInt(command[2]);
-                    if(command[1].equals("Left")) {
+                    if (command[1].equals("Left")) {
                         if (0 < index && index < weaponName.length) {
-                            Collections.swap(Arrays.asList(weaponName),index,index - 1);
+                            Collections.swap(Arrays.asList(weaponName), index, index - 1);
                         }
-                    }else {
+                    } else {
                         if (0 <= index && index < weaponName.length - 1) {
-                            Collections.swap(Arrays.asList(weaponName),index,index + 1);
+                            Collections.swap(Arrays.asList(weaponName), index, index + 1);
                         }
                     }
                     break;
                 case "Check":
-                    if(command[1].equals("Even")) {
+                    if (command[1].equals("Even")) {
                         IntStream.range(0, weaponName.length).filter(i -> i % 2 == 0)
                                 .mapToObj(i -> weaponName[i] + " ").forEach(System.out::print);
-                    }else {
+                    } else {
                         IntStream.range(0, weaponName.length).filter(i -> i % 2 != 0)
                                 .mapToObj(i -> weaponName[i] + " ").forEach(System.out::print);
                     }
