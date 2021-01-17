1203cd56360e5ffe7f4f896dd3e9a7c36aaafab3
Petrov-connect <Petrov.connect@gmail.com>
2021-01-17 16:11:46 +0200

    java code

--- Exams/Problem6.java
+++ Exams/Problem6.java
@@ -1,16 +1,20 @@
 package Exams;
 //created by J.M.
+
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Problem6 {
+
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
         String regEx = "U\\$(?<name>[A-Z][a-z]{2,})U\\$P@\\$(?<pass>[A-Za-z]{5,}\\d+)P@\\$";
         int n = Integer.parseInt(scanner.nextLine());
         Pattern pattern = Pattern.compile(regEx);
         int count = 0;
+
         for (int i = 0; i < n; i++) {
             String input = scanner.nextLine();
             Matcher matcher = pattern.matcher(input);
