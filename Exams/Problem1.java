0405dd4b4a62daa1701e39ee4f04a9b9ac31bbbc
Petrov-connect <Petrov.connect@gmail.com>
2021-01-17 16:10:50 +0200

    java code

--- Exams/Problem1.java
+++ Exams/Problem1.java
@@ -1,42 +1,47 @@
 package Exams;
 //created by J.M.
+
 import java.util.*;
+
 public class Problem1 {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
-        String text=scan.nextLine();
+        String text = scan.nextLine();
         String input;
-        while (!"Done".equals(input=scan.nextLine())){
-            String[]command=input.split("\\s+");
-            switch (command[0]){
-                case"Change":
-                    text= text.replaceAll(command[1],command[2]);
+
+        while (!"Done".equals(input = scan.nextLine())) {
+            String[] command = input.split("\\s+");
+            switch (command[0]) {
+                case "Change":
+                    text = text.replaceAll(command[1], command[2]);
                     System.out.println(text);
-                  break;
-                case"Includes":
-                    if(text.contains(command[1])){
+                    break;
+                case "Includes":
+                    if (text.contains(command[1])) {
                         System.out.println("True");
-                    }else{
+                    } else {
                         System.out.println("False");
                     }
                     break;
-                case"End":
-                    if(text.endsWith(command[1])){
-                            System.out.println("True");
-                    }else{
+                case "End":
+                    if (text.endsWith(command[1])) {
+                        System.out.println("True");
+                    } else {
                         System.out.println("False");
                     }
                     break;
-                case"Uppercase":
-                    text=text.toUpperCase();
+                case "Uppercase":
+                    text = text.toUpperCase();
                     System.out.println(text);
                     break;
-                case"FindIndex":
+                case "FindIndex":
                     System.out.println(text.indexOf(command[1]));
                     break;
-                case"Cut":
-                    text=text.substring(Integer.parseInt(command[1]),
-                            Integer.parseInt(command[1])+Integer.parseInt(command[2]));
+                case "Cut":
+                    text = text.substring(Integer.parseInt(command[1]),
+                            Integer.parseInt(command[1]) + Integer.parseInt(command[2]));
                     System.out.println(text);
                     break;
             }
