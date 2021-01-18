39becb2713ac7a16f327635ffa29479f5f2e35fb
Petrov-connect <Petrov.connect@gmail.com>
2021-01-18 21:24:49 +0200

    java code

--- Exams/ThePianist.java
+++ Exams/ThePianist.java
@@ -1,49 +1,55 @@
 package Exams;
 //created by J.M.
+
 import java.util.*;
 import java.util.function.Function;
 
 public class ThePianist {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
-        int n=Integer.parseInt(scan.nextLine());
-        Map<String,String[]>pieceInfo=new HashMap<>();
-        for (int i = 0; i <n ; i++) {
-            String[]input=scan.nextLine().split("\\|");
-            pieceInfo.put(input[0],new String[]{input[1],input[2]});
+        int n = Integer.parseInt(scan.nextLine());
+        Map<String, String[]> pieceInfo = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            String[] input = scan.nextLine().split("\\|");
+            pieceInfo.put(input[0], new String[]{input[1], input[2]});
         }
         String input;
-        while (!"Stop".equals(input=scan.nextLine())){
-            String[]command=input.split("\\|");
-            switch (command[0]){
-                case"Add":
-                    if(pieceInfo.containsKey(command[1])){
-                        System.out.printf("%s is already in the collection!%n",command[1]);
-                    }else{
-                        pieceInfo.put(command[1],new String[]{command[2],command[3]});
-                        System.out.printf("%s by %s in %s added to the collection!%n",command[1],command[2],command[3]);
+
+        while (!"Stop".equals(input = scan.nextLine())) {
+            String[] command = input.split("\\|");
+            switch (command[0]) {
+                case "Add":
+                    if (pieceInfo.containsKey(command[1])) {
+                        System.out.printf("%s is already in the collection!%n", command[1]);
+                    } else {
+                        pieceInfo.put(command[1], new String[]{command[2], command[3]});
+                        System.out.printf("%s by %s in %s added to the collection!%n", command[1], command[2], command[3]);
                     }
                     break;
-                case"Remove":
-                    if(pieceInfo.containsKey(command[1])){
+                case "Remove":
+                    if (pieceInfo.containsKey(command[1])) {
                         pieceInfo.remove(command[1]);
-                        System.out.printf("Successfully removed %s!%n",command[1]);
-                    }else{
-                        System.out.printf("Invalid operation! %s does not exist in the collection.%n",command[1]);
+                        System.out.printf("Successfully removed %s!%n", command[1]);
+                    } else {
+                        System.out.printf("Invalid operation! %s does not exist in the collection.%n", command[1]);
                     }
                     break;
-                case"ChangeKey":
-                    if(pieceInfo.containsKey(command[1])){
-                        pieceInfo.get(command[1])[1]=command[2];
-                        System.out.printf("Changed the key of %s to %s!%n",command[1],command[2]);
-                    }else{
-                        System.out.printf("Invalid operation! %s does not exist in the collection.%n",command[1]);
+                case "ChangeKey":
+                    if (pieceInfo.containsKey(command[1])) {
+                        pieceInfo.get(command[1])[1] = command[2];
+                        System.out.printf("Changed the key of %s to %s!%n", command[1], command[2]);
+                    } else {
+                        System.out.printf("Invalid operation! %s does not exist in the collection.%n", command[1]);
                     }
                     break;
             }
         }
-        pieceInfo.entrySet().stream().sorted(Comparator.comparing((Function<Map.Entry<String, String[]>, String>) Map.Entry::getKey)
-                .thenComparing(a -> a.getValue()[0])).forEach(entry->
-                System.out.printf("%s -> Composer: %s, Key: %s%n",entry.getKey(),entry.getValue()[0],entry.getValue()[1]));
+        pieceInfo.entrySet().stream()
+                .sorted(Comparator.comparing((Function<Map.Entry<String, String[]>, String>) Map.Entry::getKey)
+                .thenComparing(a -> a.getValue()[0])).forEach(entry ->
+                System.out.printf("%s -> Composer: %s, Key: %s%n", entry.getKey(), entry.getValue()[0], entry.getValue()[1]));
     }
 }
