2554ca62d85260df9beb0576aa48f8e2ac087ae4
Petrov-connect <Petrov.connect@gmail.com>
2021-01-19 22:07:03 +0200

    java code

--- Lists/ListManipulationBasics.java
+++ Lists/ListManipulationBasics.java
@@ -1,32 +1,39 @@
 package Lists;
 //created by J.M.
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 
 public class ListManipulationBasics {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
-        List<Integer> numbersList= Arrays.stream(scan.nextLine().split(" ")).map(Integer::parseInt).collect(Collectors.toList());
+        List<Integer> numbersList = Arrays.stream(scan.nextLine().split(" "))
+                .map(Integer::parseInt).collect(Collectors.toList());
+
         String input;
-        while (!("end").equals(input=scan.nextLine())){
-            String [] command=input.split(" ");
-            switch (command[0]){
-                case"Add":
+
+        while (!("end").equals(input = scan.nextLine())) {
+            String[] command = input.split(" ");
+            switch (command[0]) {
+                case "Add":
                     numbersList.add(Integer.parseInt(command[1]));
                     break;
-                case"Remove":
+                case "Remove":
                     numbersList.remove((Integer) Integer.parseInt(command[1]));
                     break;
-                case"RemoveAt":
+                case "RemoveAt":
                     numbersList.remove(Integer.parseInt(command[1]));
                     break;
-                case"Insert":
-                    numbersList.add(Integer.parseInt(command[2]),Integer.parseInt(command[1]));
+                case "Insert":
+                    numbersList.add(Integer.parseInt(command[2]), Integer.parseInt(command[1]));
                     break;
             }
         }
-        System.out.println(numbersList.toString().replaceAll("[\\[\\],]", "").replaceAll(", ", " "));
+        System.out.println(numbersList.toString().replaceAll("[\\[\\],]", "")
+                .replaceAll(", ", " "));
     }
 }
