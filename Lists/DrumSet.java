23634c533cad6bd98b8e2172a05406e4826efe34
Petrov-connect <Petrov.connect@gmail.com>
2021-01-19 22:06:11 +0200

    java code

--- Lists/DrumSet.java
+++ Lists/DrumSet.java
@@ -1,24 +1,29 @@
 package Lists;
 //created by J.M.
+
 import java.util.*;
 import java.util.stream.Collectors;
 
 public class DrumSet {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
-        double money=Double.parseDouble(scan.nextLine());
-        List<Integer> drumSet=Arrays.stream(scan.nextLine().split("\\s+")).map(Integer::parseInt).collect(Collectors.toList());
+        double money = Double.parseDouble(scan.nextLine());
+        List<Integer> drumSet = Arrays.stream(scan.nextLine().split("\\s+"))
+                .map(Integer::parseInt).collect(Collectors.toList());
         List<Integer> saveDrumSet = new ArrayList<>(drumSet);
         String input;
-        while (!"Hit it again, Gabsy!".equals(input=scan.nextLine())){
-            int hitPower=Integer.parseInt(input);
-            for (int i = 0; i <drumSet.size() ; i++) {
-                drumSet.set(i,drumSet.get(i)-hitPower);
-                if(drumSet.get(i)<=0){
-                    if(!((money-(saveDrumSet.get(i)*3.0)) <0)) {
+
+        while (!"Hit it again, Gabsy!".equals(input = scan.nextLine())) {
+            int hitPower = Integer.parseInt(input);
+            for (int i = 0; i < drumSet.size(); i++) {
+                drumSet.set(i, drumSet.get(i) - hitPower);
+                if (drumSet.get(i) <= 0) {
+                    if (!((money - (saveDrumSet.get(i) * 3.0)) < 0)) {
                         drumSet.set(i, saveDrumSet.get(i));
                         money -= saveDrumSet.get(i) * 3;
-                    }else{
+                    } else {
                         drumSet.remove(i);
                         saveDrumSet.remove(i);
                         i--;
@@ -27,6 +32,6 @@ public class DrumSet {
             }
         }
         System.out.println(drumSet.toString().replaceAll("[\\[\\],]", "").replaceAll(", ", " "));
-        System.out.printf("Gabsy has %.2flv.",money);
+        System.out.printf("Gabsy has %.2flv.", money);
     }
 }
