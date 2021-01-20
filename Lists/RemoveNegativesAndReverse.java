1f6fae2754abbbe000d81d8afef7ba8729bec8a6
Petrov-connect <Petrov.connect@gmail.com>
2021-01-20 22:50:36 +0200

    java code

--- Lists/RemoveNegativesAndReverse.java
+++ Lists/RemoveNegativesAndReverse.java
@@ -1,20 +1,26 @@
 package Lists;
 //created by J.M.
+
 import java.util.*;
 import java.util.stream.Collectors;
 
 public class RemoveNegativesAndReverse {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
-        List<Integer> numbersList=Arrays.stream(scan.nextLine().split(" ")).map(Integer::parseInt).collect(Collectors.toList());
+        List<Integer> numbersList = Arrays.stream(scan.nextLine().split(" "))
+                .map(Integer::parseInt).collect(Collectors.toList());
+
         for (int i = 0; i < numbersList.size(); i++) {
-            numbersList.removeIf(n -> n<0);
+            numbersList.removeIf(n -> n < 0);
         }
-        if(numbersList.isEmpty()){
+        if (numbersList.isEmpty()) {
             System.out.println("empty");
-        }else {
+        } else {
             Collections.reverse(numbersList);
-            System.out.println(numbersList.toString().replaceAll("[\\[\\],]", "").replaceAll(", ", " "));
+            System.out.println(numbersList.toString().replaceAll("[\\[\\],]", "")
+                    .replaceAll(", ", " "));
         }
     }
 }
