cc561e80bbf1804311153aea8e2e171cf4858bc2
Petrov-connect <Petrov.connect@gmail.com>
2021-01-20 22:51:14 +0200

    java code

--- Lists/TakeSkipRope.java
+++ Lists/TakeSkipRope.java
@@ -1,25 +1,39 @@
 package Lists;
 //created by J.M.
+
 import java.util.*;
 import java.util.stream.Collectors;
 
 public class TakeSkipRope {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
-        List<Character> text=Arrays.stream(scan.nextLine().split("")).map(a->a.charAt(0)).collect(Collectors.toList());
-        List<String> nonNumbers=new ArrayList<>();
-        List<Integer> numbers=new ArrayList<>();
-        text.forEach(character -> { if (Character.isDigit(character))
-        { numbers.add(Integer.parseInt(String.valueOf(character)));} else{ nonNumbers.add(String.valueOf(character));}});
-        StringBuilder output=new StringBuilder(); int count=0;
-        for (int i =0; i<numbers.size();  i++) {
+        List<Character> text = Arrays.stream(scan.nextLine().split("")).map(a -> a.charAt(0)).collect(Collectors.toList());
+        List<String> nonNumbers = new ArrayList<>();
+        List<Integer> numbers = new ArrayList<>();
+
+        text.forEach(character -> {
+            if (Character.isDigit(character)) {
+                numbers.add(Integer.parseInt(String.valueOf(character)));
+            } else {
+                nonNumbers.add(String.valueOf(character));
+            }
+        });
+
+        StringBuilder output = new StringBuilder();
+        int count = 0;
+
+        for (int i = 0; i < numbers.size(); i++) {
             if (i % 2 == 0) {
                 for (int j = 0; j < numbers.get(i); j++) {
-                    if(count!=nonNumbers.size()) {
-                        output.append(nonNumbers.get(count)); count++;
+                    if (count != nonNumbers.size()) {
+                        output.append(nonNumbers.get(count));
+                        count++;
                     }
                 }
-            } else { count += numbers.get(i);
+            } else {
+                count += numbers.get(i);
             }
         }
         System.out.println(output);
