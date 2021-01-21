5075a417144245b2a098a308705351fa69bf2822
Petrov-connect <Petrov.connect@gmail.com>
2021-01-21 16:16:08 +0200

    java code

--- MapsAndStreamAPI/CountCharsInString.java
+++ MapsAndStreamAPI/CountCharsInString.java
@@ -1,16 +1,20 @@
 package MapsAndStreamAPI;
 //created by J.M.
+
 import java.util.*;
 
 public class CountCharsInString {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
-        String input=scan.nextLine();
-        Map<Character,Integer>output=new LinkedHashMap<>();
+        String input = scan.nextLine();
+        Map<Character, Integer> output = new LinkedHashMap<>();
+
         for (int i = 0; i < input.length(); i++) {
             char key = input.charAt(i);
             if (key != 32) {
-            output.put(key, !output.containsKey(key) ? 1 : output.get(key) + 1);
+                output.put(key, !output.containsKey(key) ? 1 : output.get(key) + 1);
             }
         }
         output.forEach((key, value) -> System.out.printf("%c -> %d%n", key, value));
