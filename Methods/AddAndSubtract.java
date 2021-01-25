81adaf0ccad24ef594a3db58c52d36792ea6776e
Petrov-connect <Petrov.connect@gmail.com>
2021-01-25 15:39:33 +0200

    java code

--- Methods/AddAndSubtract.java
+++ Methods/AddAndSubtract.java
@@ -1,21 +1,26 @@
 package Methods;
 //created by J.M.
+
 import java.util.Scanner;
 
 public class AddAndSubtract {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int num1=Integer.parseInt(scan.nextLine());
-        int num2=Integer.parseInt(scan.nextLine());
-        int num3=Integer.parseInt(scan.nextLine());
 
-        System.out.println(subtract(sum(num1,num2),num3));
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        int num1 = Integer.parseInt(scan.nextLine());
+        int num2 = Integer.parseInt(scan.nextLine());
+        int num3 = Integer.parseInt(scan.nextLine());
+
+        System.out.println(subtract(sum(num1, num2), num3));
     }
-    static int sum(int num1,int num2){
-        return num1+num2;
+
+    static int sum(int num1, int num2) {
+        return num1 + num2;
     }
-    static int subtract(int num1,int num2){
-        return num1-num2;
+
+    static int subtract(int num1, int num2) {
+        return num1 - num2;
     }
 
 }
