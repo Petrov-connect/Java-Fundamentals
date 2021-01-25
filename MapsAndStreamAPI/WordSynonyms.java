2096da7b5ce8d6a51e9c46fafabc7f1d120f23ff
Petrov-connect <Petrov.connect@gmail.com>
2021-01-25 15:38:43 +0200

    java code

--- MapsAndStreamAPI/WordSynonyms.java
+++ MapsAndStreamAPI/WordSynonyms.java
@@ -1,12 +1,16 @@
 package MapsAndStreamAPI;
 //created by J.M.
+
 import java.util.*;
 
 public class WordSynonyms {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
-        int n=Integer.parseInt(scan.nextLine());
-        Map<String,List<String>>wordSynonym=new LinkedHashMap<>();
+        int n = Integer.parseInt(scan.nextLine());
+        Map<String, List<String>> wordSynonym = new LinkedHashMap<>();
+
         for (int i = 0; i < n; i++) {
             String word = scan.nextLine();
             String synonym = scan.nextLine();
