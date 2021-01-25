e77c17ebcb71985f6d8341a3c86c5f2c7e4901d8
Petrov-connect <Petrov.connect@gmail.com>
2021-01-25 15:40:05 +0200

    java code

--- Methods/CalculateRectangleArea.java
+++ Methods/CalculateRectangleArea.java
@@ -1,16 +1,20 @@
 package Methods;
 //created by J.M.
+
 import java.util.Scanner;
 
 public class CalculateRectangleArea {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
-        int width=Integer.parseInt(scan.nextLine());;
-        int length=Integer.parseInt(scan.nextLine());
-        System.out.println(getRectangleArea(width,length));
+        int width = Integer.parseInt(scan.nextLine());
+        int length = Integer.parseInt(scan.nextLine());
+
+        System.out.println(getRectangleArea(width, length));
     }
 
     private static int getRectangleArea(int width, int length) {
-        return width*length;
+        return width * length;
     }
 }
