262bfb023f3567603dcc39e4fd08e2da44660be2
Petrov-connect <Petrov.connect@gmail.com>
2021-01-25 15:40:16 +0200

    java code

--- Methods/Calculations.java
+++ Methods/Calculations.java
@@ -1,16 +1,20 @@
 package Methods;
 //created by J.M.
+
 import java.util.Scanner;
 
 public class Calculations {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
         String command=scan.nextLine();
         int firstNum=Integer.parseInt(scan.nextLine());
         int secondNum=Integer.parseInt(scan.nextLine());
+
         switch (command){
-            case"add": add(firstNum,secondNum); break;
-            case"divide":  divide(firstNum,secondNum); break;
+            case"add": add(firstNum,secondNum);            break;
+            case"divide":  divide(firstNum,secondNum);     break;
             case"multiply":  multiply(firstNum,secondNum); break;
             case"subtract":  subtract(firstNum,secondNum); break;
         }
