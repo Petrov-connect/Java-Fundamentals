5139d422ee17e1d3451eb491d77923d27a89d292
Petrov-connect <Petrov.connect@gmail.com>
2021-01-25 15:39:44 +0200

    java code

--- Methods/AlgorithmSort.java
+++ Methods/AlgorithmSort.java
@@ -1,18 +1,23 @@
 package Methods;
 //created by J.M.
+
 import java.util.*;
 import java.util.stream.Collectors;
 
 public class AlgorithmSort {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
         List<Integer> array = Arrays.stream(scan.nextLine().split("\\s+"))
                 .map(Integer::parseInt).collect(Collectors.toList());
+
         List<Integer> smallerNumbers = new ArrayList<>();
         int smaller = (array.stream().mapToInt(integer -> integer).min().orElse(Integer.MAX_VALUE));
         int indexSmaller = array.indexOf(smaller);
         smallerNumbers.add(smaller);
         int count = 0;
+
         for (int i = 0; i < array.size(); i++) {
             boolean nextSmaller = true;
             for (Integer integer : array) {
@@ -21,11 +26,14 @@ public class AlgorithmSort {
                     break;
                 }
             }
-            if (checkIsSmaller(array.get(i),smallerNumbers) && nextSmaller) {
+            if (checkIsSmaller(array.get(i), smallerNumbers) && nextSmaller) {
                 smallerNumbers.add(array.get(i));
                 if (indexSmaller > array.indexOf(array.get(i))) {
-                    int old = array.get(i); array.remove(i); array.add(old); count++;
-                    indexSmaller=array.indexOf(old);
+                    int old = array.get(i);
+                    array.remove(i);
+                    array.add(old);
+                    count++;
+                    indexSmaller = array.indexOf(old);
                 } else {
                     indexSmaller = array.indexOf(array.get(i));
                 }
@@ -33,8 +41,9 @@ public class AlgorithmSort {
             }
         }
         System.out.println(count);
-        System.out.println( array.stream().map(String::valueOf).collect(Collectors.joining(" ")));
+        System.out.println(array.stream().map(String::valueOf).collect(Collectors.joining(" ")));
     }
+
     private static boolean checkIsSmaller(Integer integer, List<Integer> smallerNumbers) {
         for (Integer smallerNumber : smallerNumbers) {
             if (!(integer > smallerNumber)) {
