391d0b7eddd4e354f0f51f609bb724e1c6d23bd1
Petrov-connect <Petrov.connect@gmail.com>
2021-01-26 23:43:34 +0200

    java code

--- Methods/DataType2.java
+++ Methods/DataType2.java
@@ -1,27 +1,34 @@
 package Methods;
 //created by J.M.
+
 import java.util.Scanner;
 
 public class DataType2 {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
-        String input=scan.nextLine();
-        while (!input.equals("END")){
-            boolean isInt=false; boolean isDouble=false;int count=0; int dot=0;
-            for(int i=0;i<input.length(); i++){
-                char n=input.charAt(i);
-                if(n>47&&n<58) {
+        String input = scan.nextLine();
+
+        while (!input.equals("END")) {
+            boolean isInt = false;
+            boolean isDouble = false;
+            int count = 0;
+            int dot = 0;
+            for (int i = 0; i < input.length(); i++) {
+                char n = input.charAt(i);
+                if (n > 47 && n < 58) {
                     count++;
-                }else if(n==46){
+                } else if (n == 46) {
                     dot++;
-                }else if(i==0&&n==45){
+                } else if (i == 0 && n == 45) {
                     count++;
                 }
-                if(count==input.length()){
-                    isInt=true;
-                }else if((count+1==input.length())&& dot==1){
-                        isDouble=true;
-                    }
+                if (count == input.length()) {
+                    isInt = true;
+                } else if ((count + 1 == input.length()) && dot == 1) {
+                    isDouble = true;
+                }
             }
             if (isInt) {
                 System.out.printf("%s is integer type%n", input);
@@ -29,12 +36,12 @@ public class DataType2 {
                 System.out.printf("%s is floating point type%n", input);
             } else if (input.length() == 1) {
                 System.out.printf("%s is character type%n", input);
-            } else if ("true".equals(input.toLowerCase()) || "false".equals(input.toLowerCase())) {
+            } else if ("true".equalsIgnoreCase(input) || "false".equalsIgnoreCase(input)) {
                 System.out.printf("%s is boolean type%n", input);
             } else {
                 System.out.printf("%s is string type%n", input);
             }
-            input=scan.nextLine();
+            input = scan.nextLine();
         }
     }
 }
