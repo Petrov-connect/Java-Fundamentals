5c3f75809f61aee7e8acaaee17a672fb742fd9b4
Petrov-connect <Petrov.connect@gmail.com>
2021-01-26 23:42:57 +0200

    java code

--- Methods/CharactersInRange.java
+++ Methods/CharactersInRange.java
@@ -1,18 +1,22 @@
 package Methods;
 //created by J.M.
+
 import java.util.Scanner;
 
 public class CharactersInRange {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
-        char first=scan.nextLine().charAt(0);
-        char last=scan.nextLine().charAt(0);
-        printCharactersBetween(first,last);
+        char first = scan.nextLine().charAt(0);
+        char last = scan.nextLine().charAt(0);
+
+        printCharactersBetween(first, last);
     }
 
     private static void printCharactersBetween(char first, char last) {
-        for(char a=(char)Math.min(first,last); a<Math.max(first,last)-1; a++){
-            System.out.printf("%c ",a+1);
+        for (char a = (char) Math.min(first, last); a < Math.max(first, last) - 1; a++) {
+            System.out.printf("%c ", a + 1);
         }
     }
 }
