732fbf5f8aaeb631d6fabea5046b0169a16b9606
Petrov-connect <Petrov.connect@gmail.com>
2021-01-26 23:44:05 +0200

    java code

--- Methods/FactorialDivision.java
+++ Methods/FactorialDivision.java
@@ -1,20 +1,21 @@
 package Methods;
 //created by J.M.
+
 import java.util.Scanner;
+import java.util.stream.IntStream;
 
 public class FactorialDivision {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int num1=Integer.parseInt(scan.nextLine());
-        int num2=Integer.parseInt(scan.nextLine());
 
-        System.out.printf("%.2f",(double)factorial(num1)/factorial(num2));
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        int num1 = Integer.parseInt(scan.nextLine());
+        int num2 = Integer.parseInt(scan.nextLine());
+
+        System.out.printf("%.2f", (double) factorial(num1) / factorial(num2));
     }
-    static long factorial(int num){
-        long sum=1;
-        for (int i = num; i > 1; i--) {
-            sum*=i;
-        }
-        return sum;
+
+    static long factorial(int num) {
+        return IntStream.iterate(num, i -> i > 1, i -> i - 1).asLongStream().reduce(1, (a, b) -> a * b);
     }
 }
