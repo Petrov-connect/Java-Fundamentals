4b60938a76342dc5657c13198c8e0a3c4052e523
Petrov-connect <Petrov.connect@gmail.com>
2021-01-26 23:43:09 +0200

    java code

--- Methods/DataType.java
+++ Methods/DataType.java
@@ -1,21 +1,25 @@
 package Methods;
 //created by J.M.
+
 import java.util.Scanner;
 
 public class DataType {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
         String input = scan.nextLine();
+
         while (!input.equals("END")) {
             boolean isInt = true;
             try {
-                int integer = Integer.parseInt(input);
+                Integer.parseInt(input);
             } catch (NumberFormatException e) {
                 isInt = false;
             }
             boolean isDouble = true;
             try {
-                double dbl = Double.parseDouble(input);
+                Double.parseDouble(input);
             } catch (NumberFormatException e) {
                 isDouble = false;
             }
@@ -25,12 +29,12 @@ public class DataType {
                 System.out.printf("%s is floating point type%n", input);
             } else if (input.length() == 1) {
                 System.out.printf("%s is character type%n", input);
-            } else if ("true".equals(input.toLowerCase()) || "false".equals(input.toLowerCase())) {
+            } else if ("true".equalsIgnoreCase(input) || "false".equalsIgnoreCase(input)) {
                 System.out.printf("%s is boolean type%n", input);
             } else {
                 System.out.printf("%s is string type%n", input);
             }
-            input=scan.nextLine();
+            input = scan.nextLine();
         }
     }
 }
