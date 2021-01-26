ce4de17bbd6d448c9d8debf634d243dad9fd4a8a
Petrov-connect <Petrov.connect@gmail.com>
2021-01-26 23:43:22 +0200

    java code

--- Methods/DataType1.java
+++ Methods/DataType1.java
@@ -1,17 +1,22 @@
 package Methods;
 //created by J.M.
+
 import java.util.Scanner;
 
 public class DataType1 {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
         String input = scan.nextLine();
+
         while (!"END".equals(input)) {
             Scanner readData = new Scanner(input);
             checkDataType(readData, input);
             input = scan.nextLine();
         }
     }
+
     private static void checkDataType(Scanner readData, String input) {
         if (readData.hasNextInt()) {
             System.out.printf("%s is integer type%n", input);
