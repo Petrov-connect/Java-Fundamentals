aaf1d4784e62ea769b48b41e1760e0a4107b0914
Petrov-connect <Petrov.connect@gmail.com>
2021-01-26 23:43:44 +0200

    java code

--- Methods/DataTypeFinder.java
+++ Methods/DataTypeFinder.java
@@ -1,11 +1,15 @@
 package Methods;
 //created by J.M.
+
 import java.util.Scanner;
 
 public class DataTypeFinder {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
         String input = scan.nextLine();
+
         while (!input.equals("END")) {
             Scanner newInput = new Scanner(input);
             if (newInput.hasNextBoolean()) {
@@ -16,7 +20,7 @@ public class DataTypeFinder {
                 System.out.printf("%s is character type%n", input);
             } else if (newInput.hasNextDouble()) {
                 System.out.printf("%s is floating point type%n", input);
-            } else  {
+            } else {
                 System.out.printf("%s is string type%n", input);
             }
             input = scan.nextLine();
