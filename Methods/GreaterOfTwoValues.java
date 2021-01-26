aa8e24396857aaba6c5de76bbe42c3fb31732a86
Petrov-connect <Petrov.connect@gmail.com>
2021-01-26 23:44:27 +0200

    java code

--- Methods/GreaterOfTwoValues.java
+++ Methods/GreaterOfTwoValues.java
@@ -1,29 +1,43 @@
 package Methods;
 //created by J.M.
+
 import java.util.Scanner;
 
 public class GreaterOfTwoValues {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
-        String type=scan.nextLine();
-        String first=scan.nextLine();
-        String second=scan.nextLine();
-        switch (type){
-            case"int": System.out.println(getMax(Integer.parseInt(first), Integer.parseInt(second))); break;
-            case"char": System.out.println(getMax(first.charAt(0),second.charAt(0))); break;
-            case"string": System.out.println(getMax(first,second)); break;
+        String type = scan.nextLine();
+        String first = scan.nextLine();
+        String second = scan.nextLine();
+
+        switch (type) {
+            case "int":
+                System.out.println(getMax(Integer.parseInt(first), Integer.parseInt(second)));
+                break;
+            case "char":
+                System.out.println(getMax(first.charAt(0), second.charAt(0)));
+                break;
+            case "string":
+                System.out.println(getMax(first, second));
+                break;
 
         }
     }
-    static int getMax(int first, int second){
+
+    static int getMax(int first, int second) {
         return Math.max(first, second);
     }
-    static char getMax(char first,char second){
-        return (char) Math.max(first,second);
+
+    static char getMax(char first, char second) {
+        return (char) Math.max(first, second);
     }
-    static String getMax(String first,String second){
-        if(first.compareTo(second)>=0){
+
+    static String getMax(String first, String second) {
+        if (first.compareTo(second) >= 0) {
             return first;
-        }return second;
+        }
+        return second;
     }
 }
