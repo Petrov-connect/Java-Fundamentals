e35ec75f3d3bb3412d2e594f6d63bb7b3a274f31
Petrov-connect <Petrov.connect@gmail.com>
2021-01-26 23:44:16 +0200

    java code

--- Methods/Grades.java
+++ Methods/Grades.java
@@ -1,26 +1,29 @@
 package Methods;
 //created by J.M.
+
 import java.util.Scanner;
 
 public class Grades {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
-        double grade=Double.parseDouble(scan.nextLine());
+        double grade = Double.parseDouble(scan.nextLine());
         printGradeWord(grade);
     }
 
     private static void printGradeWord(double grade) {
-        String word="";
-        if(2.00<=grade&&grade<3.00){
-            word="Fail";
-        }else if(3.00<=grade&&grade<3.50){
-            word="Poor";
-        }else if(3.50<=grade&&grade<4.50){
-            word="Good";
-        }else if(4.50<=grade&&grade<5.50){
-            word="Very good";
-        }else if(5.50<=grade&&grade<=6.00){
-            word="Excellent";
+        String word = "";
+        if (2.00 <= grade && grade < 3.00) {
+            word = "Fail";
+        } else if (3.00 <= grade && grade < 3.50) {
+            word = "Poor";
+        } else if (3.50 <= grade && grade < 4.50) {
+            word = "Good";
+        } else if (4.50 <= grade && grade < 5.50) {
+            word = "Very good";
+        } else if (5.50 <= grade && grade <= 6.00) {
+            word = "Excellent";
         }
         System.out.println(word);
     }
