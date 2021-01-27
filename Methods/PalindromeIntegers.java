b981635f8decd63f3f9a136ebca757d211e075f6
Petrov-connect <Petrov.connect@gmail.com>
2021-01-27 20:57:22 +0200

    java code

--- Methods/PalindromeIntegers.java
+++ Methods/PalindromeIntegers.java
@@ -1,11 +1,15 @@
 package Methods;
 //created by J.M.
+
 import java.util.Scanner;
 
 public class PalindromeIntegers {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
         String input = scan.nextLine();
+
         while (!input.equals("END")) {
             checkForPalindrome(input);
             input = scan.nextLine();
