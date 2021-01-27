5d5b75965f9cae71f249da50c99afcdd14e0cabe
Petrov-connect <Petrov.connect@gmail.com>
2021-01-27 20:56:26 +0200

    java code

--- Methods/MultiplicationSign.java
+++ Methods/MultiplicationSign.java
@@ -1,21 +1,23 @@
 package Methods;
 //created by J.M.
+
 import java.util.Scanner;
 
 public class MultiplicationSign {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int num1=Integer.parseInt(scan.nextLine());
-        int num2=Integer.parseInt(scan.nextLine());
-        int num3=Integer.parseInt(scan.nextLine());
 
-        if (num1==0 || num2==0 || num3==0) {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        int num1 = Integer.parseInt(scan.nextLine());
+        int num2 = Integer.parseInt(scan.nextLine());
+        int num3 = Integer.parseInt(scan.nextLine());
+
+        if (num1 == 0 || num2 == 0 || num3 == 0) {
             System.out.println("zero");
-        }
-        else if ((num1>0 && num2>0 && num3>0) || (num1<0 && num2<0 && num3>0) || (num1<0 && num2>0 && num3<0) || (num1>0 && num2<0 && num3<0)) {
+        } else if ((num1 > 0 && num2 > 0 && num3 > 0) || (num1 < 0 && num2 < 0 && num3 > 0) || (num1 < 0 && num2 > 0 && num3 < 0)
+                || (num1 > 0 && num2 < 0 && num3 < 0)) {
             System.out.println("positive");
-        }
-        else {
+        } else {
             System.out.println("negative");
         }
     }
