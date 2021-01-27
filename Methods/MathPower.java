ec4279cc3e53c352859ca0b10edf1c2191a2eb49
Petrov-connect <Petrov.connect@gmail.com>
2021-01-27 20:56:00 +0200

    java code

--- Methods/MathPower.java
+++ Methods/MathPower.java
@@ -1,16 +1,21 @@
 package Methods;
 //created by J.M.
+
 import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class MathPower {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
         double number = Double.parseDouble(scan.nextLine());
         int powerNumber = Integer.parseInt(scan.nextLine());
+
         System.out.println(new DecimalFormat("0.####").format(MathPowerNumber(number, powerNumber)));
     }
+
     static double MathPowerNumber(double number, int powerNumber) {
-        return Math.pow(number,powerNumber);
+        return Math.pow(number, powerNumber);
     }
 }
