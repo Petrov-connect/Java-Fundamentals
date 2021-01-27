aa534160a2fa7572aa279bd50b6588f9e7b49f4b
Petrov-connect <Petrov.connect@gmail.com>
2021-01-27 20:56:11 +0200

    java code

--- Methods/MiddleCharacters.java
+++ Methods/MiddleCharacters.java
@@ -1,19 +1,23 @@
 package Methods;
 //created by J.M.
+
 import java.util.Scanner;
 
 public class MiddleCharacters {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
-        String input=scan.nextLine();
+        String input = scan.nextLine();
+
         printTheMiddleCharacter(input);
     }
 
     private static void printTheMiddleCharacter(String input) {
-        if(input.length()%2==0){
-            System.out.println(input.charAt(input.length()/2-1)+""+input.charAt(input.length()/2));
-        }else{
-            System.out.println(input.charAt(input.length()/2));
+        if (input.length() % 2 == 0) {
+            System.out.println(input.charAt(input.length() / 2 - 1) + "" + input.charAt(input.length() / 2));
+        } else {
+            System.out.println(input.charAt(input.length() / 2));
         }
     }
 }
