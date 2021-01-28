eebe86191d808aa13e603f8398141e9eb31bccc8
Petrov-connect <Petrov.connect@gmail.com>
2021-01-28 23:37:00 +0200

    java code

--- MidExam/BonusScoringSystem.java
+++ MidExam/BonusScoringSystem.java
@@ -1,21 +1,28 @@
 package MidExam;
 //created by J.M.
+
 import java.util.*;
 
 public class BonusScoringSystem {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
-        int students=Integer.parseInt(scan.nextLine());
-        int lectures=Integer.parseInt(scan.nextLine());
-        int additionalBonus= Integer.parseInt(scan.nextLine());
-            double max = 0;double maxBonus=0;
-            for (int i = 0; i < students; i++) {
-                double attendances = Double.parseDouble(scan.nextLine());
-                if (attendances > max) {
-                    max = attendances; maxBonus=((attendances / lectures) * (5 + additionalBonus));
-                }
+        int students = Integer.parseInt(scan.nextLine());
+        int lectures = Integer.parseInt(scan.nextLine());
+        int additionalBonus = Integer.parseInt(scan.nextLine());
+
+        double max = 0;
+        double maxBonus = 0;
+
+        for (int i = 0; i < students; i++) {
+            double attendances = Double.parseDouble(scan.nextLine());
+            if (attendances > max) {
+                max = attendances;
+                maxBonus = ((attendances / lectures) * (5 + additionalBonus));
             }
-            System.out.printf("Max Bonus: %.0f.%n", Math.ceil(maxBonus));
-            System.out.printf("The student has attended %.0f lectures.", max);
+        }
+        System.out.printf("Max Bonus: %.0f.%n", Math.ceil(maxBonus));
+        System.out.printf("The student has attended %.0f lectures.", max);
     }
 }
