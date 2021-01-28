87f76afe2cae1de43fa7632a0759ce2aae96b478
Petrov-connect <Petrov.connect@gmail.com>
2021-01-28 23:35:14 +0200

    java code

--- Methods/PrintingTriangle.java
+++ Methods/PrintingTriangle.java
@@ -1,28 +1,33 @@
 package Methods;
 //created by J.M.
+
 import java.util.Scanner;
 
 public class PrintingTriangle {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
-        int triangleLength=Integer.parseInt(scan.nextLine());
+        int triangleLength = Integer.parseInt(scan.nextLine());
+
         printingTriangle(triangleLength);
     }
 
     private static void printingTriangle(int triangleLength) {
-        for(int i=1; i<=triangleLength; i++){
+        for (int i = 1; i <= triangleLength; i++) {
             printing(i);
             System.out.println();
-            }
-        for (int i = triangleLength-1; i >=1; i--) {
+        }
+        for (int i = triangleLength - 1; i >= 1; i--) {
             printing(i);
             System.out.println();
         }
     }
+
     private static void printing(int end) {
         for (int j = 1; j <= end; j++) {
             System.out.print(j);
-            if(j!=end){
+            if (j != end) {
                 System.out.print(" ");
             }
         }
