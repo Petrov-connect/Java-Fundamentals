cbe4a4b7ab9e2ef2fcda3aa9b1e46ea768f60687
Petrov-connect <Petrov.connect@gmail.com>
2021-01-28 23:35:25 +0200

    java code

--- Methods/RepeatString.java
+++ Methods/RepeatString.java
@@ -1,17 +1,21 @@
 package Methods;
 //created by J.M.
+
 import java.util.Scanner;
 
 public class RepeatString {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
-        String s=scan.nextLine();
-        int repetition=Integer.parseInt(scan.nextLine());
-        System.out.println(repeatString(s,repetition));
+        String s = scan.nextLine();
+        int repetition = Integer.parseInt(scan.nextLine());
+
+        System.out.println(repeatString(s, repetition));
     }
 
     private static StringBuilder repeatString(String s, int count) {
-        StringBuilder output=new StringBuilder();
+        StringBuilder output = new StringBuilder();
         output.append(String.valueOf(s).repeat(count));
         return output;
     }
