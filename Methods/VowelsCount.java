1c0fba895f1373d925e53eb8e9b20a50f524e739
Petrov-connect <Petrov.connect@gmail.com>
2021-01-28 23:36:18 +0200

    java code

--- Methods/VowelsCount.java
+++ Methods/VowelsCount.java
@@ -1,19 +1,24 @@
 package Methods;
 //created by J.M.
+
 import java.util.Scanner;
 
 public class VowelsCount {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
-        String input=scan.nextLine();
+        String input = scan.nextLine();
+
         printCountOfVowels(input);
     }
 
     private static void printCountOfVowels(String input) {
-        int count=0;
-        for(int i=0; i<input.length(); i++){
-            char next=input.charAt(i);
-            if(next=='a'||next=='o'||next=='e'||next=='u'||next=='i'||next=='A'||next=='E'||next=='O'||next=='U'||next=='I'){
+        int count = 0;
+        for (int i = 0; i < input.length(); i++) {
+            char next = input.charAt(i);
+            if (next == 'a' || next == 'o' || next == 'e' || next == 'u' || next == 'i' || next == 'A'
+                    || next == 'E' || next == 'O' || next == 'U' || next == 'I') {
                 count++;
             }
         }
