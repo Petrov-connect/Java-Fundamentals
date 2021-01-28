a004b2604581ef222dbd8e8f422b1757f13d1281
Petrov-connect <Petrov.connect@gmail.com>
2021-01-28 23:35:02 +0200

    java code

--- Methods/PasswordValidator.java
+++ Methods/PasswordValidator.java
@@ -1,14 +1,19 @@
 package Methods;
 //created by J.M.
+
 import java.util.Scanner;
 
 public class PasswordValidator {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
-        String password=scan.nextLine();
-        boolean check1= checkLengthPassword(password);
-        boolean check2=checkForLettersAndDigits(password);
-        boolean check3= checkForTwoDigit(password);
+        String password = scan.nextLine();
+
+        boolean check1 = checkLengthPassword(password);
+        boolean check2 = checkForLettersAndDigits(password);
+        boolean check3 = checkForTwoDigit(password);
+
         if (check1 && check2 && check3) {
             System.out.println("Password is valid");
         }
@@ -23,28 +28,31 @@ public class PasswordValidator {
             return false;
         }
     }
-     static boolean checkForLettersAndDigits(String password) {
-        boolean trueFalse=true;
+
+    static boolean checkForLettersAndDigits(String password) {
+        boolean trueFalse = true;
         for (int i = 0; i < password.length(); i++) {
-             if (!Character.isDigit(password.charAt(i)) && !Character.isLetter(password.charAt(i))) {
-                 System.out.println("Password must consist only of letters and digits");
-                 trueFalse=false; break;
-             }
-         }
+            if (!Character.isDigit(password.charAt(i)) && !Character.isLetter(password.charAt(i))) {
+                System.out.println("Password must consist only of letters and digits");
+                trueFalse = false;
+                break;
+            }
+        }
         return trueFalse;
     }
-     static boolean checkForTwoDigit(String password) {
-         int count = 0;
-         for (int i = 0; i < password.length(); i++) {
-             if (Character.isDigit(password.charAt(i))) {
-                 count++;
-             }
-         }
-         if (count >= 2) {
-             return true;
-         } else {
-             System.out.println("Password must have at least 2 digits");
-             return false;
-         }
+
+    static boolean checkForTwoDigit(String password) {
+        int count = 0;
+        for (int i = 0; i < password.length(); i++) {
+            if (Character.isDigit(password.charAt(i))) {
+                count++;
+            }
+        }
+        if (count >= 2) {
+            return true;
+        } else {
+            System.out.println("Password must have at least 2 digits");
+            return false;
+        }
     }
 }
