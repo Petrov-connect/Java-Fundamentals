2ea189aabd33415f5de6211e36afe1fc8baf4636
Petrov-connect <Petrov.connect@gmail.com>
2021-01-28 23:35:35 +0200

    java code

--- Methods/SignOfIntegerNumbers.java
+++ Methods/SignOfIntegerNumbers.java
@@ -1,24 +1,26 @@
 package Methods;
 //created by J.M.
+
 import java.util.Scanner;
 
 public class SignOfIntegerNumbers {
+
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
-        int n=Integer.parseInt(scan.nextLine());
+        int n = Integer.parseInt(scan.nextLine());
 
-        System.out.printf("The number %d is %s.",n,getSign(n));
+        System.out.printf("The number %d is %s.", n, getSign(n));
     }
 
     static String getSign(int n) {
         String output;
-        if(n>0){
-            output="positive";
-        }else if(n<0){
-            output="negative";
-        }else{
-           output="zero";
+        if (n > 0) {
+            output = "positive";
+        } else if (n < 0) {
+            output = "negative";
+        } else {
+            output = "zero";
         }
         return output;
     }
