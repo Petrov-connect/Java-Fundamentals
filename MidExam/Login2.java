5e56a3931320eaf8d8a2acb4b6d00ce163b22ae5
Petrov-connect <Petrov.connect@gmail.com>
2021-01-30 00:55:21 +0200

    java code

--- MidExam/Login2.java
+++ MidExam/Login2.java
@@ -1,20 +1,24 @@
 package MidExam;
 //created by J.M.
+
 import java.util.Scanner;
 
 public class Login2 {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
         String username = scan.nextLine();
-        String password="";
-        for(int i=username.length()-1;i>=0;i--){
-            password+=username.charAt(i);
+        StringBuilder password = new StringBuilder();
+
+        for (int i = username.length() - 1; i >= 0; i--) {
+            password.append(username.charAt(i));
         }
         String inputPassword = scan.nextLine();
-        int count=1;
-        while (!inputPassword.equals(password)) {
-            if(count==4){
-                System.out.printf("User %s blocked!%n",username);
+        int count = 1;
+        while (!inputPassword.equals(password.toString())) {
+            if (count == 4) {
+                System.out.printf("User %s blocked!%n", username);
                 return;
             }
             System.out.printf("Incorrect password. Try again.%n");
