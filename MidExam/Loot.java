ba8ff123688d93a21ea2296569971e9979d71081
Petrov-connect <Petrov.connect@gmail.com>
2021-01-30 00:56:12 +0200

    java code

--- MidExam/Loot.java
+++ MidExam/Loot.java
@@ -1,26 +1,30 @@
 package MidExam;
 //created by J.M.
+
 import java.util.*;
 
 public class Loot {
+
     public static void main(String[] args) {
-        Scanner scan = new Scanner (System.in);
+
+        Scanner scan = new Scanner(System.in);
         String[] itemsText = scan.nextLine().split("\\|");
         List<String> items = new ArrayList<>(Arrays.asList(itemsText));
         String input;
-        while (!"Yohoho!".equals(input=scan.nextLine())) {
+
+        while (!"Yohoho!".equals(input = scan.nextLine())) {
             String[] command = input.split("\\s+", 2);
-            if(command[0].equals("MidExam.Loot")) {
+            if (command[0].equals("MidExam.Loot")) {
                 Arrays.stream(command[1].split("\\s+")).filter(s -> !items.contains(s)).forEach(s -> items.add(0, s));
-            }else if(command[0].equals("Drop")&& (Integer.parseInt(command[1])>=0&&Integer.parseInt(command[1])<items.size())){
+            } else if (command[0].equals("Drop") && (Integer.parseInt(command[1]) >= 0 && Integer.parseInt(command[1]) < items.size())) {
                 items.add(items.remove(Integer.parseInt(command[1])));
-            }else{
-                    List<String> stolen = new ArrayList<>();
-                    while (stolen.size() < Integer.parseInt(command[1]) && !items.isEmpty()) {
-                        stolen.add(items.remove(items.size() - 1));
-                    }
+            } else {
+                List<String> stolen = new ArrayList<>();
+                while (stolen.size() < Integer.parseInt(command[1]) && !items.isEmpty()) {
+                    stolen.add(items.remove(items.size() - 1));
+                }
                 Collections.reverse(stolen);
-                System.out.println(String.join(", ",stolen));
+                System.out.println(String.join(", ", stolen));
             }
         }
         if (items.isEmpty()) {
