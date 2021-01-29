691c49c805f9a6497888810f7de7342458c95e73
Petrov-connect <Petrov.connect@gmail.com>
2021-01-29 14:14:18 +0200

    java code

--- MidExam/DeckOfCards.java
+++ MidExam/DeckOfCards.java
@@ -1,21 +1,25 @@
 package MidExam;
 //created by J.M.
+
 import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class DeckOfCards {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
         List<String> list = Arrays.stream(scan.nextLine().split(", ")).collect(Collectors.toList());
         int n = Integer.parseInt(scan.nextLine());
+
         for (int i = 0; i < n; i++) {
             List<String> commands = Arrays.stream(scan.nextLine().split(", ")).collect(Collectors.toList());
-            switch (commands.get(0)){
+            switch (commands.get(0)) {
                 case "Add":
-                    if (list.contains(commands.get(1))){
+                    if (list.contains(commands.get(1))) {
                         System.out.println("Card is already bought");
-                    }else {
+                    } else {
                         System.out.println("Card successfully bought");
                         list.add(commands.get(1));
                     }
@@ -24,33 +28,38 @@ public class DeckOfCards {
                     if (list.contains(commands.get(1))) {
                         list.remove(commands.get(1));
                         System.out.println("Card successfully sold");
-                    }else {
+                    } else {
                         System.out.println("Card not found");
                     }
                     break;
                 case "Remove At":
-                    if (Integer.parseInt(commands.get(1)) > list.size() - 1 || Integer.parseInt(commands.get(1)) < 0){
+                    if (Integer.parseInt(commands.get(1)) > list.size() - 1 || Integer.parseInt(commands.get(1)) < 0) {
                         System.out.println("Index out of range");
-                    }else {
+                    } else {
                         System.out.println("Card successfully sold");
                         list.remove(Integer.parseInt(commands.get(1)));
                     }
                     break;
                 case "Insert":
-                    if (!(Integer.parseInt(commands.get(1)) > list.size() - 1 || Integer.parseInt(commands.get(1)) < 0)){
-                        if (!(list.contains(commands.get(2)))){
+                    if (!(Integer.parseInt(commands.get(1)) > list.size() - 1 || Integer.parseInt(commands.get(1)) < 0)) {
+                        if (!(list.contains(commands.get(2)))) {
                             System.out.println("Card successfully bought");
                             list.add(Integer.parseInt(commands.get(1)), commands.get(2));
 
-                        }else {
+                        } else {
                             System.out.println("Card is already bought");
                         }
-                    }else {
+                    } else {
                         System.out.println("Index out of range");
                     }
             }
         }
-        IntStream.range(0, list.size()).forEach(i -> { if (i == list.size() - 1) { System.out.print(list.get(i));
-        } else { System.out.print(list.get(i) + ", "); } });
+        IntStream.range(0, list.size()).forEach(i -> {
+            if (i == list.size() - 1) {
+                System.out.print(list.get(i));
+            } else {
+                System.out.print(list.get(i) + ", ");
+            }
+        });
     }
 }
