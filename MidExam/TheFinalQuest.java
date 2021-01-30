177a51a3b89ab465bc9dbb951415e8e8de50ea4a
Petrov-connect <Petrov.connect@gmail.com>
2021-01-31 01:49:05 +0200

    java code

--- MidExam/TheFinalQuest.java
+++ MidExam/TheFinalQuest.java
@@ -1,42 +1,47 @@
 package MidExam;
 //created by J.M.
+
 import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class TheFinalQuest {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
-        List<String>message=Arrays.stream(scan.nextLine().split("\\s+")).collect(Collectors.toList());
+        List<String> message = Arrays.stream(scan.nextLine().split("\\s+")).collect(Collectors.toList());
         String input;
-        while (!"Stop".equals(input=scan.nextLine())){
-            String[]command=input.split("\\s+");
-            switch (command[0].trim()){
-                case"Delete":
-                    int indexToRemove=Integer.parseInt(command[1])+1;
-                    if(indexToRemove>=0&&indexToRemove<message.size()){
-                            message.remove(indexToRemove);
+
+        while (!"Stop".equals(input = scan.nextLine())) {
+            String[] command = input.split("\\s+");
+            switch (command[0].trim()) {
+                case "Delete":
+                    int indexToRemove = Integer.parseInt(command[1]) + 1;
+                    if (indexToRemove >= 0 && indexToRemove < message.size()) {
+                        message.remove(indexToRemove);
                     }
                     break;
-                case"Swap":
-                    if(message.contains(command[1])&&message.contains(command[2]))
-                        Collections.swap(message,message.indexOf(command[1]),message.indexOf(command[2]));
+                case "Swap":
+                    if (message.contains(command[1]) && message.contains(command[2]))
+                        Collections.swap(message, message.indexOf(command[1]), message.indexOf(command[2]));
                     break;
-                case"Put":
-                    int indexToPut=Integer.parseInt(command[2])-1;
-                    if(indexToPut>=0&&indexToPut<=message.size()){
-                            message.add(indexToPut, command[1]);
+                case "Put":
+                    int indexToPut = Integer.parseInt(command[2]) - 1;
+                    if (indexToPut >= 0 && indexToPut <= message.size()) {
+                        message.add(indexToPut, command[1]);
                     }
                     break;
-                case"Sort":
-                    Collections.sort(message); Collections.reverse(message);
+                case "Sort":
+                    Collections.sort(message);
+                    Collections.reverse(message);
                     break;
-                case"Replace":
+                case "Replace":
                     IntStream.range(0, message.size()).filter(i -> message.get(i).equals(command[2]))
                             .forEach(i -> message.set(i, command[1]));
                     break;
             }
         }
-        System.out.println(String.join(" ",message));
+        System.out.println(String.join(" ", message));
     }
 }
