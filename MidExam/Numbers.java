b8e681eac3ad568b5a531ac2914d37f1ee4665b8
Petrov-connect <Petrov.connect@gmail.com>
2021-01-31 01:47:49 +0200

    java code

--- MidExam/Numbers.java
+++ MidExam/Numbers.java
@@ -1,13 +1,16 @@
 package MidExam;
 //created by J.M.
+
 import java.util.*;
 import java.util.stream.Collectors;
 
 public class Numbers {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
         List<Integer> input = Arrays.stream(scan.nextLine().split("\\s+")).map(Integer::parseInt).collect(Collectors.toList());
-        List <Integer>averageNumbers=input.stream().filter(e->e>input.stream()
+        List<Integer> averageNumbers = input.stream().filter(e -> e > input.stream()
                 .mapToDouble(Integer::doubleValue).average().orElse(0.0))
                 .sorted(Comparator.reverseOrder()).limit(5).collect(Collectors.toList());
         if (averageNumbers.isEmpty()) {
