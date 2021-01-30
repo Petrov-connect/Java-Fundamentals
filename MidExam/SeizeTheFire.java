c90f5ee367e0c21ff0470ed5b4f58638f2fdbbea
Petrov-connect <Petrov.connect@gmail.com>
2021-01-31 01:48:15 +0200

    java code

--- MidExam/SeizeTheFire.java
+++ MidExam/SeizeTheFire.java
@@ -1,15 +1,22 @@
 package MidExam;
 //created by J.M.
+
 import java.util.*;
 import java.util.stream.Collectors;
 
 public class SeizeTheFire {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
-        List<String>cells=Arrays.stream(scan.nextLine().split("#")).collect(Collectors.toList());
-        int water=Integer.parseInt(scan.nextLine());
-        double effort=0.0; int totalFire=0;
-        List<String>output=new ArrayList<>(); output.add(0,"Cells:");
+        List<String> cells = Arrays.stream(scan.nextLine().split("#")).collect(Collectors.toList());
+        int water = Integer.parseInt(scan.nextLine());
+
+        double effort = 0.0;
+        int totalFire = 0;
+        List<String> output = new ArrayList<>();
+        output.add(0, "Cells:");
+
         for (String cell : cells) {
             boolean isValid = false;
             String[] input = cell.split(" = ");
@@ -21,11 +28,14 @@ public class SeizeTheFire {
             } else if ((input[0].equals("Low")) && (firePower > 0 && firePower < 51)) {
                 isValid = true;
             }
-            if(isValid&&water>=firePower){
-                water-=firePower; effort+=firePower*0.25; output.add(" - "+firePower); totalFire+=firePower;
+            if (isValid && water >= firePower) {
+                water -= firePower;
+                effort += firePower * 0.25;
+                output.add(" - " + firePower);
+                totalFire += firePower;
             }
         }
         output.forEach(System.out::println);
-        System.out.printf("Effort: %.2f%nTotal Fire: %d",effort,totalFire);
+        System.out.printf("Effort: %.2f%nTotal Fire: %d", effort, totalFire);
     }
 }
