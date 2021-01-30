ea35b650f13d4f1f16bbb3ab15a73c029056a2d5
Petrov-connect <Petrov.connect@gmail.com>
2021-01-31 01:48:53 +0200

    java code

--- MidExam/SoftUniReception.java
+++ MidExam/SoftUniReception.java
@@ -1,19 +1,24 @@
 package MidExam;
 //created by J.M.
+
 import java.util.*;
 
 public class SoftUniReception {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
-        int employeeEfficiency=scan.nextInt()+scan.nextInt()+scan.nextInt();
-        int studentsCount=scan.nextInt();
-        int hours=0;
-        while (studentsCount>0){
+        int employeeEfficiency = scan.nextInt() + scan.nextInt() + scan.nextInt();
+
+        int studentsCount = scan.nextInt();
+        int hours = 0;
+
+        while (studentsCount > 0) {
             hours++;
-            if(hours%4!=0){
-                studentsCount-=employeeEfficiency;
+            if (hours % 4 != 0) {
+                studentsCount -= employeeEfficiency;
             }
         }
-        System.out.printf("Time needed: %dh.",hours);
+        System.out.printf("Time needed: %dh.", hours);
     }
 }
