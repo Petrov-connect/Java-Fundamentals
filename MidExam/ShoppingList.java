ae88284bf6f8d9a3eb79f4903bc0959af34d2d8e
Petrov-connect <Petrov.connect@gmail.com>
2021-01-31 01:48:41 +0200

    java code

--- MidExam/ShoppingList.java
+++ MidExam/ShoppingList.java
@@ -1,37 +1,41 @@
 package MidExam;
 //created by J.M.
+
 import java.util.*;
 import java.util.stream.Collectors;
 
 public class ShoppingList {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
-        List<String> shoppingList= Arrays.stream((scan.nextLine().split("!+"))).collect(Collectors.toList());
+        List<String> shoppingList = Arrays.stream((scan.nextLine().split("!+"))).collect(Collectors.toList());
         String input;
-        while (!"Go Shopping!".equals(input=scan.nextLine())){
-            String [] command=input.split(" ");
-            switch (command[0]){
-                case"Urgent":
-                    if(!shoppingList.contains(command[1])){
-                        shoppingList.add(0,command[1]);
+
+        while (!"Go Shopping!".equals(input = scan.nextLine())) {
+            String[] command = input.split(" ");
+            switch (command[0]) {
+                case "Urgent":
+                    if (!shoppingList.contains(command[1])) {
+                        shoppingList.add(0, command[1]);
                     }
                     break;
-                case"Unnecessary":
+                case "Unnecessary":
                     shoppingList.remove(command[1]);
                     break;
-                case"Correct":
-                    if(shoppingList.contains(command[1])){
-                        shoppingList.set(shoppingList.indexOf(command[1]),command[2]);
+                case "Correct":
+                    if (shoppingList.contains(command[1])) {
+                        shoppingList.set(shoppingList.indexOf(command[1]), command[2]);
                     }
                     break;
-                case"Rearrange":
-                    if(shoppingList.contains(command[1])){
+                case "Rearrange":
+                    if (shoppingList.contains(command[1])) {
                         shoppingList.remove(command[1]);
                         shoppingList.add(command[1]);
                     }
                     break;
             }
         }
-                System.out.println(shoppingList.toString().replaceAll("[\\[\\]]", ""));
+        System.out.println(shoppingList.toString().replaceAll("[\\[\\]]", ""));
     }
 }
