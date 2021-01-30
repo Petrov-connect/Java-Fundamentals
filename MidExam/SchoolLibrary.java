210700d9f9d9ea6a90ad4653dbf8af2c9253301e
Petrov-connect <Petrov.connect@gmail.com>
2021-01-31 01:48:03 +0200

    java code

--- MidExam/SchoolLibrary.java
+++ MidExam/SchoolLibrary.java
@@ -1,36 +1,40 @@
 package MidExam;
 //created by J.M.
+
 import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class SchoolLibrary {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
-        List<String> bookList= Arrays.stream((scan.nextLine().split("&+"))).collect(Collectors.toList());
-        String input;
+        List<String> bookList = Arrays.stream((scan.nextLine().split("&+"))).collect(Collectors.toList());
         IntStream.range(0, bookList.size()).forEach(i -> bookList.set(i, bookList.get(i).trim()));
-        while (!"Done".equals(input=scan.nextLine())){
-            String[] command=input.split("\\|");
-            switch (command[0].trim()){
-                case"Add Book":
-                    if(!bookList.contains(command[1].trim())) {
-                        bookList.add(0,command[1].trim());
+        String input;
+
+        while (!"Done".equals(input = scan.nextLine())) {
+            String[] command = input.split("\\|");
+            switch (command[0].trim()) {
+                case "Add Book":
+                    if (!bookList.contains(command[1].trim())) {
+                        bookList.add(0, command[1].trim());
                     }
-                break;
-                case"Take Book":
+                    break;
+                case "Take Book":
                     bookList.remove(command[1].trim());
                     break;
-                case"Swap Books":
-                    if(bookList.contains(command[1].trim())&&bookList.contains(command[2].trim())){
-                        Collections.swap(bookList,bookList.indexOf(command[1].trim()),bookList.indexOf(command[2].trim()));
+                case "Swap Books":
+                    if (bookList.contains(command[1].trim()) && bookList.contains(command[2].trim())) {
+                        Collections.swap(bookList, bookList.indexOf(command[1].trim()), bookList.indexOf(command[2].trim()));
                     }
                     break;
-                case"Insert Book":
+                case "Insert Book":
                     bookList.add(command[1].trim());
                     break;
-                case"Check Book":
-                    if(Integer.parseInt(command[1].trim())>=0&&Integer.parseInt(command[1].trim())<bookList.size()){
+                case "Check Book":
+                    if (Integer.parseInt(command[1].trim()) >= 0 && Integer.parseInt(command[1].trim()) < bookList.size()) {
                         System.out.println(bookList.get(Integer.parseInt(command[1].trim())));
                     }
                     break;
