ad4e6f3113991198f8bffccaf124ac46d6a1263e
Petrov-connect <Petrov.connect@gmail.com>
2021-02-01 00:07:46 +0200

    java code

--- ObjectsAndClasses/BigFactorial.java
+++ ObjectsAndClasses/BigFactorial.java
@@ -1,16 +1,19 @@
 package ObjectsAndClasses;
 //created by J.M.
+
 import java.math.BigInteger;
 import java.util.Scanner;
 
 public class BigFactorial {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
-        int n=Integer.parseInt(scan.nextLine());
-        BigInteger f=new BigInteger(String.valueOf(1));
+        int n = Integer.parseInt(scan.nextLine());
+        BigInteger f = new BigInteger(String.valueOf(1));
 
         for (int i = n; i > 1; i--) {
-            f=f.multiply(BigInteger.valueOf(Integer.parseInt(String.valueOf(i))));
+            f = f.multiply(BigInteger.valueOf(Integer.parseInt(String.valueOf(i))));
         }
         System.out.println(f);
     }
