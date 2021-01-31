b5e4197246cca546a503941f3d675b7968b37e5c
Petrov-connect <Petrov.connect@gmail.com>
2021-02-01 00:07:31 +0200

    java code

--- ObjectsAndClasses/Articles2.java
+++ ObjectsAndClasses/Articles2.java
@@ -1,34 +1,36 @@
 package ObjectsAndClasses;
 //created by J.M.
+
 import java.util.*;
 
 public class Articles2 {
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
-        List<Article> dataList=new ArrayList<>();
-        int n=Integer.parseInt(scan.nextLine());
-        for (int i = 0; i <n; i++) {
-            String[] input=scan.nextLine().split(", ");
-            Article data=new Article(input[0],input[1],input[2]);
+        List<Article> dataList = new ArrayList<>();
+        int n = Integer.parseInt(scan.nextLine());
+
+        for (int i = 0; i < n; i++) {
+            String[] input = scan.nextLine().split(", ");
+            Article data = new Article(input[0], input[1], input[2]);
             dataList.add(data);
         }
-        String input=scan.nextLine();
+        String input = scan.nextLine();
 
-        switch (input){
-            case"title":
-                dataList.stream().sorted(Comparator.comparing(Article::getTitle)).forEach(data->System.out.println(data.output()));
+        switch (input) {
+            case "title":
+                dataList.stream().sorted(Comparator.comparing(Article::getTitle)).forEach(data -> System.out.println(data.output()));
                 break;
-            case"content":
-                dataList.stream().sorted(Comparator.comparing(Article::getContent)).forEach(data->System.out.println(data.output()));
+            case "content":
+                dataList.stream().sorted(Comparator.comparing(Article::getContent)).forEach(data -> System.out.println(data.output()));
                 break;
-            case"author":
-                dataList.stream().sorted(Comparator.comparing(Article::getAuthor)).forEach(data->System.out.println(data.output()));
+            case "author":
+                dataList.stream().sorted(Comparator.comparing(Article::getAuthor)).forEach(data -> System.out.println(data.output()));
                 break;
         }
     }
 
-    static class Article{
+    static class Article {
         String title;
         String content;
         String author;
@@ -38,6 +40,7 @@ public class Articles2 {
             this.content = content;
             this.author = author;
         }
+
         public String getTitle() {
             return title;
         }
@@ -50,8 +53,8 @@ public class Articles2 {
             return author;
         }
 
-        public String output(){
-            return String.format("%s - %s: %s",getTitle(),getContent(),getAuthor());
+        public String output() {
+            return String.format("%s - %s: %s", getTitle(), getContent(), getAuthor());
         }
     }
 }
