549b2ed1ff9aa42e7e26a12d77e9f0d1e6e65628
Petrov-connect <Petrov.connect@gmail.com>
2021-02-02 22:08:16 +0200

    java code

--- ObjectsAndClasses/RandomizeWords.java
+++ ObjectsAndClasses/RandomizeWords.java
@@ -1,21 +1,27 @@
 package ObjectsAndClasses;
 //created by J.M.
+
 import java.util.Arrays;
+
 import static java.lang.System.*;
+
 import java.util.Random;
 import java.util.Scanner;
 
 public class RandomizeWords {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(in);
-        String [] words= scan.nextLine().split(" ");
-        Random randomGenerator=new Random();
+        String[] words = scan.nextLine().split(" ");
+        Random randomGenerator = new Random();
+
         for (int i = 0; i < words.length; i++) {
-            int x= randomGenerator.nextInt(words.length);
-            int y=randomGenerator.nextInt(words.length);
-            String old=words[x];
-            words[x]=words[y];
-            words[y]=old;
+            int x = randomGenerator.nextInt(words.length);
+            int y = randomGenerator.nextInt(words.length);
+            String old = words[x];
+            words[x] = words[y];
+            words[y] = old;
         }
         Arrays.stream(words).forEach(System.out::println);
     }
