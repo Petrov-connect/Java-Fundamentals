bb64fea39ac813c8d24ae46edeb061104cda853f
Petrov-connect <Petrov.connect@gmail.com>
2021-02-02 22:08:40 +0200

    java code

--- ObjectsAndClasses/RawData2.java
+++ ObjectsAndClasses/RawData2.java
@@ -1,16 +1,20 @@
 package ObjectsAndClasses;
 //created by J.M.
+
 import java.util.*;
 
 public class RawData2 {
+
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
         int n = Integer.parseInt(scanner.nextLine());
         List<Car> carList = new ArrayList<>();
+
         while (n-- > 0) {
             String[] input = scanner.nextLine().split("\\s+");
             String model = input[0];
-            Engine engine= new Engine(Integer.parseInt(input[1]), Integer.parseInt(input[2]));
+            Engine engine = new Engine(Integer.parseInt(input[1]), Integer.parseInt(input[2]));
             Cargo cargo = new Cargo(Integer.parseInt(input[3]), input[4]);
             Tire tire1 = new Tire(Double.parseDouble(input[5]), Integer.parseInt(input[6]));
             Tire tire2 = new Tire(Double.parseDouble(input[7]), Integer.parseInt(input[8]));
@@ -21,12 +25,19 @@ public class RawData2 {
         }
         String command = scanner.nextLine();
         if (command.equals("fragile")) {
-            carList.stream().filter(e -> e.getCargo().getType().equals("fragile")).forEach(e -> { Tire[] tires = e.getTires();
-            if (Arrays.stream(tires).anyMatch(tireSet -> tireSet.getPressure() < 1)) { System.out.println(e.getModel()); } });
+            carList.stream().filter(e -> e.getCargo().getType().equals("fragile")).forEach(e -> {
+                Tire[] tires = e.getTires();
+                if (Arrays.stream(tires).anyMatch(tireSet -> tireSet.getPressure() < 1)) {
+                    System.out.println(e.getModel());
+                }
+            });
 
         } else {
-            carList.forEach(e -> { if (e.getCargo().getType().equals("flamable") && e.getEngine().getPower() > 250)
-            { System.out.println(e.getModel()); } });
+            carList.forEach(e -> {
+                if (e.getCargo().getType().equals("flamable") && e.getEngine().getPower() > 250) {
+                    System.out.println(e.getModel());
+                }
+            });
         }
     }
 
@@ -38,6 +49,7 @@ public class RawData2 {
         Tire tire2;
         Tire tire3;
         Tire tire4;
+
         public Car(String model, Engine engine, Cargo cargo, Tire tire1, Tire tire2, Tire tire3, Tire tire4) {
             this.model = model;
             this.engine = engine;
@@ -47,39 +59,63 @@ public class RawData2 {
             this.tire3 = tire3;
             this.tire4 = tire4;
         }
-        Cargo getCargo() { return cargo; }
-        Tire[] getTires() { return new Tire[]{tire1, tire2, tire3, tire4}; }
-        String getModel() { return model; }
-        Engine getEngine() { return engine; }
+
+        Cargo getCargo() {
+            return cargo;
+        }
+
+        Tire[] getTires() {
+            return new Tire[]{tire1, tire2, tire3, tire4};
+        }
+
+        String getModel() {
+            return model;
+        }
+
+        Engine getEngine() {
+            return engine;
+        }
     }
 
     static class Engine {
         int speed;
         int power;
+
         Engine(int speed, int power) {
             this.speed = speed;
             this.power = power;
         }
-         int getPower() { return power; }
+
+        int getPower() {
+            return power;
+        }
     }
 
     static class Cargo {
         int weight;
         String type;
+
         Cargo(int weight, String type) {
             this.weight = weight;
             this.type = type;
         }
-        public String getType() { return type; }
+
+        public String getType() {
+            return type;
+        }
     }
 
     static class Tire {
         double pressure;
         int age;
+
         Tire(double pressure, int age) {
             this.pressure = pressure;
             this.age = age;
         }
-        double getPressure() { return pressure; }
+
+        double getPressure() {
+            return pressure;
+        }
     }
 }
