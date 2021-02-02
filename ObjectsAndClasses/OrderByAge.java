c8dfa7eed82fefb570c6ee350fdbbd794ee08e17
Petrov-connect <Petrov.connect@gmail.com>
2021-02-02 22:07:52 +0200

    java code

--- ObjectsAndClasses/OrderByAge.java
+++ ObjectsAndClasses/OrderByAge.java
@@ -1,21 +1,25 @@
 package ObjectsAndClasses;
 //created by J.M.
+
 import java.util.*;
 
 public class OrderByAge {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
-        List<Ordered> dataList=new ArrayList<>();
+        List<Ordered> dataList = new ArrayList<>();
         String input;
-        while (!"End".equals(input=scan.nextLine())){
-           String[] inputData=input.split(" ");
-           Ordered data=new Ordered(inputData[0],inputData[1],Integer.parseInt(inputData[2]));
-           dataList.add(data);
+
+        while (!"End".equals(input = scan.nextLine())) {
+            String[] inputData = input.split(" ");
+            Ordered data = new Ordered(inputData[0], inputData[1], Integer.parseInt(inputData[2]));
+            dataList.add(data);
         }
-        dataList.stream().sorted(Comparator.comparingDouble(Ordered::getAge)).forEach(data->System.out.println(data.output()));
+        dataList.stream().sorted(Comparator.comparingDouble(Ordered::getAge)).forEach(data -> System.out.println(data.output()));
     }
 
-    static class Ordered{
+    static class Ordered {
         String name;
         String id;
         int age;
@@ -37,8 +41,9 @@ public class OrderByAge {
         public int getAge() {
             return age;
         }
-        public  String output(){
-            return String.format("%s with ID: %s is %d years old.",getName(),getId(),getAge());
+
+        public String output() {
+            return String.format("%s with ID: %s is %d years old.", getName(), getId(), getAge());
         }
     }
 }
