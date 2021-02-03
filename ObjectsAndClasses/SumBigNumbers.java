cdc19c84b33cf8cc35a04bbbb304c8cd28ede644
Petrov-connect <Petrov.connect@gmail.com>
2021-02-03 17:42:42 +0200

    java code

--- ObjectsAndClasses/SumBigNumbers.java
+++ ObjectsAndClasses/SumBigNumbers.java
@@ -1,13 +1,17 @@
 package ObjectsAndClasses;
 //created by J.M.
+
 import java.math.BigInteger;
 import java.util.Scanner;
 
 public class SumBigNumbers {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
-        BigInteger firstNumber=new BigInteger(scan.nextLine());
-        BigInteger secondNumber=new BigInteger(scan.nextLine());
+        BigInteger firstNumber = new BigInteger(scan.nextLine());
+        BigInteger secondNumber = new BigInteger(scan.nextLine());
+
         System.out.println(firstNumber.add(secondNumber));
     }
 }
