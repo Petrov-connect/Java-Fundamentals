e276e8d2e0b7e7f63536fc8b481d5490b1e1cd23
Petrov-connect <Petrov.connect@gmail.com>
2021-02-03 17:45:22 +0200

    java code

--- RegularExpressions/MatchPhoneNumber.java
+++ RegularExpressions/MatchPhoneNumber.java
@@ -1,20 +1,24 @@
 package RegularExpressions;
 //created by J.M.
+
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class MatchPhoneNumber {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
-        String regex="\\+359([ -])2(\\1)[0-9]{3}(\\1)[0-9]{4}\\b";
-        String input=scan.nextLine();
-        Pattern pattern=Pattern.compile(regex);
-        Matcher matcher=pattern.matcher(input);
-        List<String>output=new ArrayList<>();
+        String regex = "\\+359([ -])2(\\1)[0-9]{3}(\\1)[0-9]{4}\\b";
+        String input = scan.nextLine();
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(input);
+        List<String> output = new ArrayList<>();
+
         while (matcher.find()) {
-          output.add(matcher.group());
+            output.add(matcher.group());
         }
-        System.out.println(String.join(", ",output));
+        System.out.println(String.join(", ", output));
     }
 }
