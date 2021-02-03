e3b2fdc6d499faeef21a9659629708c35c3a9496
Petrov-connect <Petrov.connect@gmail.com>
2021-02-03 17:42:08 +0200

    java code

--- ObjectsAndClasses/Students.java
+++ ObjectsAndClasses/Students.java
@@ -1,30 +1,53 @@
 package ObjectsAndClasses;
 //created by J.M.
+
 import java.util.*;
 
 public class Students {
 
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
-        List<Student>students=new ArrayList<>();
+        List<Student> students = new ArrayList<>();
         String input;
-        while (!"end".equals(input=scan.nextLine())){
-            String[] data=input.split(" ");
-            Student useData=new Student(data[0],data[1],Integer.parseInt(data[2]),data[3]);
+
+        while (!"end".equals(input = scan.nextLine())) {
+            String[] data = input.split(" ");
+            Student useData = new Student(data[0], data[1], Integer.parseInt(data[2]), data[3]);
             students.add(useData);
         }
-        String townName=scan.nextLine();
+        String townName = scan.nextLine();
         for (Student student : students) {
-            if(townName.equals(student.getHometown())){
-                System.out.printf("%s %s is %d years old%n",student.getFirstName(),student.getLastName(),student.getAge());
+            if (townName.equals(student.getHometown())) {
+                System.out.printf("%s %s is %d years old%n", student.getFirstName(), student.getLastName(), student.getAge());
             }
         }
     }
-    static class Student{
-        String firstName; public String getFirstName() { return firstName; }
-        String lastName;  public String getLastName() { return lastName; }
-        int age;          public int getAge() { return age; }
-        String hometown;  public String getHometown() { return hometown; }
+
+    static class Student {
+        String firstName;
+
+        public String getFirstName() {
+            return firstName;
+        }
+
+        String lastName;
+
+        public String getLastName() {
+            return lastName;
+        }
+
+        int age;
+
+        public int getAge() {
+            return age;
+        }
+
+        String hometown;
+
+        public String getHometown() {
+            return hometown;
+        }
 
         public Student(String firstName, String lastName, int age, String hometown) {
             this.firstName = firstName;
