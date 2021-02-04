63e7642875db58c02275c0ddcb813cf9b191a4fd
Petrov-connect <Petrov.connect@gmail.com>
2021-02-05 00:04:55 +0200

    java code

--- TextProcessing/CaesarCipher.java
+++ TextProcessing/CaesarCipher.java
@@ -1,13 +1,17 @@
 package TextProcessing;
 //created by J.M.
+
 import java.util.*;
 
 public class CaesarCipher {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
-        String input=scan.nextLine();
-        for (char c: input.toCharArray()) {
-            System.out.print((char)(c+3));
+        String input = scan.nextLine();
+
+        for (char c : input.toCharArray()) {
+            System.out.print((char) (c + 3));
         }
     }
 }
