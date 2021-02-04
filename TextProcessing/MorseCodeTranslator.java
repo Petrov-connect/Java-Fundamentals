5c9dd0d1723a5bd9d582652ee23ce161a655a962
Petrov-connect <Petrov.connect@gmail.com>
2021-02-05 00:06:20 +0200

    java code

--- TextProcessing/MorseCodeTranslator.java
+++ TextProcessing/MorseCodeTranslator.java
@@ -1,9 +1,12 @@
 package TextProcessing;
 //created by J.M.
+
 import java.util.*;
 
 public class MorseCodeTranslator {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
         Map<String,String>morse=new HashMap<>(){{put( ".-","A");put( "-...","B");put( "-.-.","C");put( "-..","D");put( ".","E");
             put( "..-.","F");put( "--.","G");put( "....","H");put("..","I");put( ".---","J");put( "-.-","K");put( ".-..","L");
@@ -11,6 +14,7 @@ public class MorseCodeTranslator {
             put( "-","T");put( "..-","U");put( "...-","V");put( ".--","W");put( "-..-","X");put( "-.--","Y");put( "--..","Z");}};
 
         String[]input=scan.nextLine().split("\\s+\\|\\s+");
+
         for (String s : input) {
             StringBuilder output=new StringBuilder();
             String[]splitInput=s.split("\\s+");
