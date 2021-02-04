b2dab2cbf9ae53e8636fff5fbcc1bcb79a02df87
Petrov-connect <Petrov.connect@gmail.com>
2021-02-05 00:05:17 +0200

    java code

--- TextProcessing/DigitsLettersAndOther.java
+++ TextProcessing/DigitsLettersAndOther.java
@@ -1,14 +1,19 @@
 package TextProcessing;
 //created by J.M.
+
 import java.util.*;
 
 public class DigitsLettersAndOther {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
-        String input= scan.nextLine();
-        StringBuilder digits=new StringBuilder();
-        StringBuilder letters=new StringBuilder();
-        StringBuilder others=new StringBuilder();
+        String input = scan.nextLine();
+
+        StringBuilder digits = new StringBuilder();
+        StringBuilder letters = new StringBuilder();
+        StringBuilder others = new StringBuilder();
+
         for (int i = 0; i < input.length(); i++) {
             if (Character.isDigit(input.charAt(i))) {
                 digits.append(input.charAt(i));
@@ -18,6 +23,6 @@ public class DigitsLettersAndOther {
                 others.append(input.charAt(i));
             }
         }
-        System.out.printf("%s%n%s%n%s%n",digits,letters,others);
+        System.out.printf("%s%n%s%n%s%n", digits, letters, others);
     }
 }
