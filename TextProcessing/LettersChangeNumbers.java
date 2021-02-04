d34b766da40eff50627b6707dab305e3ce799480
Petrov-connect <Petrov.connect@gmail.com>
2021-02-05 00:05:59 +0200

    java code

--- TextProcessing/LettersChangeNumbers.java
+++ TextProcessing/LettersChangeNumbers.java
@@ -1,12 +1,17 @@
 package TextProcessing;
 //created by J.M.
+
 import java.util.*;
 
 public class LettersChangeNumbers {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
         String[] input = scan.nextLine().split("\\s+");
+
         double sum = 0;
+
         for (String s : input) {
             double number = Double.parseDouble(s.substring(1, s.length() - 1));
             int first = (Character.toLowerCase(s.charAt(0)) - 97) + 1;
