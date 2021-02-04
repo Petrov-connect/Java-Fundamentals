6478238abf2657b06a3affa5074246cd6ba00340
Petrov-connect <Petrov.connect@gmail.com>
2021-02-05 00:05:38 +0200

    java code

--- TextProcessing/ExtractPersonInformation.java
+++ TextProcessing/ExtractPersonInformation.java
@@ -1,15 +1,20 @@
 package TextProcessing;
 //created by J.M.
+
 import java.util.*;
 
 public class ExtractPersonInformation {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
-        int n=Integer.parseInt(scan.nextLine());
-        for (int i = 0; i <n ; i++) {
-            String input=(scan.nextLine());
-            System.out.printf("%s is %s years old.%n", input.substring((input.indexOf("@")+1)
-                    ,input.indexOf("|")),input.substring((input.indexOf("#")+1),input.indexOf("*")));
+        int n = Integer.parseInt(scan.nextLine());
+
+        for (int i = 0; i < n; i++) {
+            String input = (scan.nextLine());
+
+            System.out.printf("%s is %s years old.%n", input.substring((input.indexOf("@") + 1)
+                    , input.indexOf("|")), input.substring((input.indexOf("#") + 1), input.indexOf("*")));
         }
     }
 }
