7daf1132fede86ddca55b42769c5ba9d7a99011e
Petrov-connect <Petrov.connect@gmail.com>
2021-02-07 09:56:54 +0200

    java code

--- TextProcessing/WinningTicket.java
+++ TextProcessing/WinningTicket.java
@@ -1,48 +1,59 @@
 package TextProcessing;
 //created by J.M.
+
 import java.util.*;
 import java.util.stream.IntStream;
 
 public class WinningTicket {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        String[]input=scan.nextLine().split(",\\s+");
-        IntStream.range(0, input.length).forEach(i -> input[i] = input[i].trim());
-        for (String s : input) {
-            if(s.length()==20){
-                String left=getLeftRight(s.substring(0,10));
-                String right=getLeftRight(s.substring(10));
-                if((left.charAt(0)==right.charAt(0))&&(left.length()>5)&&(right.length()>5)){
-                    String jackpot="";
-                    if(left.length()==10&&right.length()==10){
-                        jackpot="Jackpot!";
-                    }
-                    System.out.printf("ticket \"%s\" - %d%c %s%n",s,Math.min(left.length(),right.length()),left.charAt(0),jackpot);
 
-                }else{
-                    System.out.printf("ticket \"%s\" - no match%n",s);
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        String[] input = scan.nextLine().split(",\\s+");
+        IntStream.range(0, input.length).forEach(i -> input[i] = input[i].trim());
+
+        for (String s : input) {
+            if (s.length() == 20) {
+                String left = getLeftRight(s.substring(0, 10));
+                String right = getLeftRight(s.substring(10));
+                if ((left.charAt(0) == right.charAt(0)) && (left.length() > 5) && (right.length() > 5)) {
+                    String jackpot = "";
+                    if (left.length() == 10 && right.length() == 10) {
+                        jackpot = "Jackpot!";
+                    }
+                    System.out.printf("ticket \"%s\" - %d%c %s%n", s, Math.min(left.length(), right.length()), left.charAt(0), jackpot);
+
+                } else {
+                    System.out.printf("ticket \"%s\" - no match%n", s);
                 }
-            }else{
+            } else {
                 System.out.println("invalid ticket");
             }
         }
     }
-    static String getLeftRight(String stringToCheck){
-        StringBuilder output=new StringBuilder();
+
+    static String getLeftRight(String stringToCheck) {
+        StringBuilder output = new StringBuilder();
         char symbol;
-        for (int i = 0; i <stringToCheck.length(); i++) {
-            if(stringToCheck.charAt(i)=='@'||stringToCheck.charAt(i)=='#'||stringToCheck.charAt(i)=='$'||stringToCheck.charAt(i)=='^'){
-                symbol=stringToCheck.charAt(i); output.append(symbol);
-                for (int j = i+1; j < stringToCheck.length(); j++) {
-                    if(stringToCheck.charAt(j)==symbol) {
+        for (int i = 0; i < stringToCheck.length(); i++) {
+            if (stringToCheck.charAt(i) == '@' || stringToCheck.charAt(i) == '#' || stringToCheck.charAt(i) == '$' || stringToCheck.charAt(i) == '^') {
+                symbol = stringToCheck.charAt(i);
+                output.append(symbol);
+                for (int j = i + 1; j < stringToCheck.length(); j++) {
+                    if (stringToCheck.charAt(j) == symbol) {
                         output.append(stringToCheck.charAt(j));
-                        }else{  break;
+                    } else {
+                        break;
                     }
                 }
-            }if(output.length()>5){ return String.valueOf(output);
-                }else{
-                if(i>4){ break;
-                }output=new StringBuilder();
+            }
+            if (output.length() > 5) {
+                return String.valueOf(output);
+            } else {
+                if (i > 4) {
+                    break;
+                }
+                output = new StringBuilder();
             }
         }
         return " ";
