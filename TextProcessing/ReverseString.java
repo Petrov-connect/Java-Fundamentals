0677020cb2c309e7de61d7a910d8e16f136b5646
Petrov-connect <Petrov.connect@gmail.com>
2021-02-07 09:55:43 +0200

    java code

--- TextProcessing/ReverseString.java
+++ TextProcessing/ReverseString.java
@@ -1,11 +1,15 @@
 package TextProcessing;
 //created by J.M.
+
 import java.util.Scanner;
 
 public class ReverseString {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
-        StringBuilder input= new StringBuilder(scan.nextLine());
+        StringBuilder input = new StringBuilder(scan.nextLine());
+
         System.out.println(input.reverse());
     }
 }
