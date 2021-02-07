28b19585d512f6fabd438eb1813e2e4d046193e7
Petrov-connect <Petrov.connect@gmail.com>
2021-02-07 09:56:06 +0200

    java code

--- TextProcessing/Substring.java
+++ TextProcessing/Substring.java
@@ -1,15 +1,19 @@
 package TextProcessing;
 //created by J.M.
+
 import java.util.*;
 
 public class Substring {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
-        String key=scan.nextLine();
-        StringBuilder output=new StringBuilder(scan.nextLine());
+        String key = scan.nextLine();
+        StringBuilder output = new StringBuilder(scan.nextLine());
         int index;
-        while ((index=output.indexOf(key))!=-1){
-            output.delete(index,index+key.length());
+
+        while ((index = output.indexOf(key)) != -1) {
+            output.delete(index, index + key.length());
         }
         System.out.println(output);
     }
