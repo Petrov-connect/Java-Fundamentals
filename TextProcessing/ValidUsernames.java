2ba1c41fed9fa14e334cc43ba50583d391aee8dd
Petrov-connect <Petrov.connect@gmail.com>
2021-02-07 09:56:42 +0200

    java code

--- TextProcessing/ValidUsernames.java
+++ TextProcessing/ValidUsernames.java
@@ -1,16 +1,20 @@
 package TextProcessing;
 //created by J.M.
+
 import java.util.*;
 
 public class ValidUsernames {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
-        String[] input=scan.nextLine().split(",\\s");
+        String[] input = scan.nextLine().split(",\\s");
+
         for (String s : input) {
             boolean isValid = true;
-            if (s.length() <3 || s.length() > 16) {
-                isValid=false;
-            }else{
+            if (s.length() < 3 || s.length() > 16) {
+                isValid = false;
+            } else {
                 for (int j = 0; j < s.length(); j++) {
                     if (!Character.isLetterOrDigit(s.charAt(j)) && s.charAt(j) != '-' && s.charAt(j) != '_') {
                         isValid = false;
