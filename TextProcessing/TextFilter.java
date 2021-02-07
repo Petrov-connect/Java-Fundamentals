7d20814bfb88c0b85cce022dad8f4d21cdd135fe
Petrov-connect <Petrov.connect@gmail.com>
2021-02-07 09:56:19 +0200

    java code

--- TextProcessing/TextFilter.java
+++ TextProcessing/TextFilter.java
@@ -1,14 +1,18 @@
 package TextProcessing;
 //created by J.M.
+
 import java.util.*;
 
 public class TextFilter {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
         String[] bannedWords = scan.nextLine().split(", ");
-        String input=scan.nextLine();
+        String input = scan.nextLine();
+
         for (String bannedWord : bannedWords) {
-            input=input.replaceAll(bannedWord, new String(new char[bannedWord.length()]).replace('\0', '*'));
+            input = input.replaceAll(bannedWord, new String(new char[bannedWord.length()]).replace('\0', '*'));
         }
         System.out.println(input);
     }
