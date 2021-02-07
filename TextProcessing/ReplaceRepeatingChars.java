e6a4c01a9fe3058fd183ddb826a6e3e380558d48
Petrov-connect <Petrov.connect@gmail.com>
2021-02-07 09:55:31 +0200

    java code

--- TextProcessing/ReplaceRepeatingChars.java
+++ TextProcessing/ReplaceRepeatingChars.java
@@ -1,14 +1,18 @@
 package TextProcessing;
 //created by J.M.
+
 import java.util.*;
 
 public class ReplaceRepeatingChars {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
-        StringBuilder input=new StringBuilder(scan.nextLine());
-        for (int i = 0; i <input.length()-1 ; i++) {
-            if(input.charAt(i)==input.charAt(i+1)){
-                input.deleteCharAt(i+1);
+        StringBuilder input = new StringBuilder(scan.nextLine());
+
+        for (int i = 0; i < input.length() - 1; i++) {
+            if (input.charAt(i) == input.charAt(i + 1)) {
+                input.deleteCharAt(i + 1);
                 i--;
             }
         }
