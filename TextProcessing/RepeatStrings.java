ceab7aa0bef503fb6754c3636713b0400e4ca480
Petrov-connect <Petrov.connect@gmail.com>
2021-02-07 09:55:18 +0200

    java code

--- TextProcessing/RepeatStrings.java
+++ TextProcessing/RepeatStrings.java
@@ -1,12 +1,16 @@
 package TextProcessing;
 //created by J.M.
+
 import java.util.*;
 
 public class RepeatStrings {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
-        String[] input=scan.nextLine().split("\\s+");
-        StringBuilder result=new StringBuilder();
+        String[] input = scan.nextLine().split("\\s+");
+        StringBuilder result = new StringBuilder();
+
         for (String s : input) {
             result.append(s.repeat(s.length()));
         }
