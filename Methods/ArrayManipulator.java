dc122e36eb236a0b39a97664145c10001d2dce57
Petrov-connect <Petrov.connect@gmail.com>
2021-02-23 20:34:21 +0200

    java code

--- Methods/ArrayManipulator.java
+++ Methods/ArrayManipulator.java
@@ -52,7 +52,7 @@ public class ArrayManipulator {
     }
     private static String getLastOddEvenNumbers(int[] arr, String[] command) {
         StringBuilder arrOutput= new StringBuilder();
-        int countOdd=Integer.parseInt(command[1]); int countEven=Integer.parseInt(command[1]);
+        int countOdd=Integer.parseInt(command[1]) , countEven=Integer.parseInt(command[1]);
         if (command[2].equals("odd")) {
             for (int i=arr.length-1; i>=0; i--) {
                 if ((arr[i] % 2 != 0)&&(countOdd>0)) {
@@ -75,7 +75,7 @@ public class ArrayManipulator {
     }
     private static String getFirstOddEvenNumbers(int[] arr,String[] command) {
         StringBuilder arrOutput = new StringBuilder();
-        int countOdd = Integer.parseInt(command[1]); int countEven=Integer.parseInt(command[1]);
+        int countOdd = Integer.parseInt(command[1]) , countEven=Integer.parseInt(command[1]);
         if (command[2].equals("odd")) {
             for (int value : arr) {
                 if ((value % 2 != 0)&&(countOdd>0)) {
@@ -92,37 +92,31 @@ public class ArrayManipulator {
         return String.valueOf(arrOutput);
     }
     private static int getMinEvenOddIndex(int[] arr,String []command) {
-        int minOddIndex=-1; int minEvenIndex=-1; int minOdd=Integer.MAX_VALUE; int minEven=Integer.MAX_VALUE;
+        int minOddIndex=-1 , minEvenIndex=-1 , minOdd=Integer.MAX_VALUE , minEven=Integer.MAX_VALUE;
         for (int i = 0; i < arr.length; i++) {
-            if((arr[i]%2!=0)&&(arr[i]<=minOdd)){
-                minOdd=arr[i]; minOddIndex=i;
-            }else if((arr[i]%2==0)&&(arr[i]<=minEven)){
-                minEven=arr[i]; minEvenIndex=i;
+            if((arr[i] % 2 !=0) && (arr[i] <= minOdd)){
+                minOdd = arr[i]; minOddIndex = i;
+            }else if((arr[i] % 2 == 0) && (arr[i] <= minEven)){
+                minEven = arr[i]; minEvenIndex = i;
             }
-        } if(command[1].equals("odd")){
-            return minOddIndex;
-        }else{
-            return minEvenIndex;
         }
+        return command[1].equals("odd") ? minOddIndex : minEvenIndex;
     }
     private static int getMaxEvenOddIndex(int[] arr,String []command) {
-        int maxOddIndex = -1; int maxEvenIndex = -1; int maxEven = Integer.MIN_VALUE; int maxOdd = Integer.MIN_VALUE;
+        int maxOddIndex = -1 ,  maxEvenIndex = -1 , maxEven = Integer.MIN_VALUE ,  maxOdd = Integer.MIN_VALUE;
         for (int i = 0; i < arr.length; i++) {
             if ((arr[i] % 2 == 0)&&(arr[i] >= maxEven)) {
                 maxEven = arr[i]; maxEvenIndex = i;
             } else if((arr[i] % 2 != 0)&&(arr[i] >= maxOdd)){
                 maxOdd = arr[i]; maxOddIndex = i;
             }
-        } if (command[1].equals("odd")) {
-            return maxOddIndex;
-        } else {
-            return maxEvenIndex;
         }
+        return command[1].equals("odd") ? maxOddIndex : maxEvenIndex;
     }
     private static void exchangeArray(int[] arr, int exchangeIndex) {
             int [] arrDuplicate=new int [arr.length]; int a=arr.length-1;
-            for (int i = exchangeIndex; i >=0; i--) {
-                arrDuplicate[a]=arr[i]; a--;
+            for (int i = exchangeIndex; i >= 0; i--) {
+                arrDuplicate[a] = arr[i]; a--;
             }
             a=0;
             for (int i = exchangeIndex+1; i < arr.length; i++) {
