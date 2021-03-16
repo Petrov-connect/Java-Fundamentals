8d75f920a01d00e6388976aef74d0e0dcf0a8a5e
Petrov-connect <Petrov.connect@gmail.com>
2021-03-16 15:40:07 +0200

    java code

--- Arrays/RecursiveFibonacci.java
+++ Arrays/RecursiveFibonacci.java
@@ -5,26 +5,32 @@ import java.util.Scanner;
 
 public class RecursiveFibonacci {
 
+    public static long[] fibonacciInfo;
+
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
         int n = Integer.parseInt(scan.nextLine());
 
-        int[] arr = new int[n];
+        fibonacciInfo= new long[n];
+        fibonacciInfo[0] = 1;
+        fibonacciInfo[1] = 1;
+        System.out.println(Fibonacci(n));
 
+    }
+
+    static long Fibonacci(long n) {
+        long currentFibonacci;
         if (n == 0) {
-            System.out.println(0);
-            return;
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        } else if (fibonacciInfo[(int) n] != 0) {
+            return fibonacciInfo[(int) n];
+        } else {
+            currentFibonacci = Fibonacci(n - 1) + Fibonacci(n - 2);
+            fibonacciInfo[(int) n] = currentFibonacci;
+            return currentFibonacci;
         }
-        arr[0] = 1;
-        if (n != 1) {
-            arr[1] = 1;
-        }
-        if (n > 2) {
-            for (int i = 2; i < n; i++) {
-                arr[i] = arr[i - 1] + arr[i - 2];
-            }
-        }
-        System.out.println(arr[n - 1]);
     }
 }
