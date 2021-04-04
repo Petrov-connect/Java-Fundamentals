ad50683faa2d586a8a60bdbc3c1e67ebbf901cf2
Petrov-connect <Petrov.connect@gmail.com>
2021-04-05 02:03:34 +0300

    java code

--- Arrays/ArrayRotation.java
+++ Arrays/ArrayRotation.java
@@ -11,6 +11,7 @@ public class ArrayRotation {
         String[] arr = scan.nextLine().split(" ");
 
         int n = Integer.parseInt(scan.nextLine());
+
         for (int i = 0; i < n; i++) {
             String[] newArr = new String[arr.length];
             newArr[newArr.length - 1] = arr[0];
